bca6d9fc523b308f6779dc5a7430eea18370a9c5
Dmitri Meshin <dmitri@blox.io>
2021-05-11 17:36:10 +0300

    Test

--- cypress/integration/ssv/add-operator.spec.js
+++ cypress/integration/ssv/add-operator.spec.js
@@ -67,11 +67,13 @@ context('Add Validator', () => {
       cy.waitFor('.bn-onboard-modal-select-wallets > :nth-child(1) > .bn-onboard-custom');
       cy.get('.bn-onboard-modal-content-header-heading').should('contain.text', 'Select a Wallet');
       cy.get('.bn-onboard-modal-select-wallets > :nth-child(1) > .bn-onboard-custom').click();
-      cy.pause();
+
+      cy.waitFor('.MuiAlert-message').should('contain.text', 'Wallet is connected!');
 
       cy.get('[data-testid="terms-and-conditions-checkbox"]').click();
       cy.waitFor('[data-testid="final-register-button"]');
       cy.get('[data-testid="final-register-button"]').click();
+
       cy.waitFor('.MuiAlert-message');
       cy.get('.MuiAlert-message').should('contain.text', 'You successfully added operator!');
     });

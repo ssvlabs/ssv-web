6677eaca73eec123bce623551531eb227a160163
guy muroch <guym@blox.io>
2021-05-13 18:06:21 +0300

    change the test to validat keys of 2048 bit

--- src/lib/crypto/Encryption/__test__/Encryption.test.ts
+++ src/lib/crypto/Encryption/__test__/Encryption.test.ts
@@ -1,29 +1,7 @@
 import JSEncrypt from 'jsencrypt';
 import Encryption, { EncryptShare } from '~lib/crypto/Encryption/Encryption';
 import Threshold, { ISharesKeyPairs } from '~lib/crypto/Threshold';
-
-const operatorPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
-                            MIICXQIBAAKBgQDMQ6q8kgHF2LEhGmnlaoKGIxJx2Bh/fb0vFTq0DPeeJgCNeYPJ
-                            2ClQ316p+EMBjXAY7bC81Y2u/FGubuAzfH0JC9ENnxnlBopnSYcCzNuxRe/MwIrs
-                            rLc1krHNpgFMYrTmsPjtB6cmA454SfdYO1lvcxUxCOdNQZc3PwxK8EHyoQIDAQAB
-                            AoGATiTU/K8e3oG3weJJAOtuY8KnG8aAGMYRyiFlA9yyHl6Ld5Q1RtLbe4T4wi2n
-                            9MAXUnIcWyGXwonk9caVHx1Q96TysDgkoALEsmq0so/bSgQGC53aUwrZEVdiwph4
-                            T0F+hLl8ov5eynHuiYMHFRgJdhHoco4NNKzCqZ0MX+KBVsECQQD/e0Mn6BLhPBOK
-                            o0fCkwYP20CsRPeU35gvUNEfak8FWz1fbFaf7n3d40j6lyABVQx2EjEDsZGhN6jl
-                            1dq/awhLAkEAzK3LVdFZgOh+t8rDU1T77DyTjexCJpXQ2E89mHL4MzlAMLKwRL31
-                            QAr3QiMJWTxHQ0aW6EvCjLGqc3Q5JY31QwJBAMM2z4jFtu9t9UyhGSsfJqmlEhTQ
-                            GhIii+nTqgeENt9T6WBpqwNHu9t5WYFJSsZZ00zA97znyOxUWHVOZHiRc2MCQQCP
-                            Tof1uDSQmzhN+vuzluckSm2NiwPt/CtTqHeaC7VYOBeHgTUFjHLwujzQ47Mh9aB3
-                            rC7wykqXM7YCTDfO4Yv9AkBP2igHYwm9Ly88sg4EOt+FvVhqzwjzldfotDFbb6Ly
-                            gECRZsqYkgW3Xzu74ZOs+L+jC3fmI4zYNSx3M4Vufy55
-                            -----END RSA PRIVATE KEY-----`;
-
-const operatorPublicKey = `-----BEGIN RSA PUBLIC KEY-----
-                            MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDMQ6q8kgHF2LEhGmnlaoKGIxJx
-                            2Bh/fb0vFTq0DPeeJgCNeYPJ2ClQ316p+EMBjXAY7bC81Y2u/FGubuAzfH0JC9EN
-                            nxnlBopnSYcCzNuxRe/MwIrsrLc1krHNpgFMYrTmsPjtB6cmA454SfdYO1lvcxUx
-                            COdNQZc3PwxK8EHyoQIDAQAB
-                            -----END RSA PUBLIC KEY-----`;
+import { operatorPrivateKey, operatorPublicKey } from './RsaKeys';
 
 describe('Check Encryption shares', () => {
     it('should return encrypt shares without error', async () => {
---
+++ src/lib/crypto/Encryption/__test__/RsaKeys.ts
@@ -0,0 +1,39 @@
+const operatorPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+                            MIIEowIBAAKCAQEAzoF3SsDDgXZB8+KjfTDsLYHRsMCq4yodylPRN3Tx6uAEHhPz
+                            AT4oO3yJsAkDKBD2QOAui/1fWQKl+w9jyWFugumFF+pbciDmG2TFzfILrX/vGKG+
+                            WGQD/vCLQ1Oqf9AgZuAK+qxp1IfjLTNFk8TmeNA8ub+pT5QAIetCnjMTqaG1FXv1
+                            5B9ScCbefbCz49mqt2HQo8mjp91C2W0Bu4+lRByYN2wz3Z7GUx7NTqCH0yfyRwKG
+                            PYb2nuOFpqDM5BF/eZVmjhKz3Bmx7UKA70WKVSA/vD7VeEuxtZAsAA0aO2wKxyRg
+                            +tdQKo3JiqNlzOyZfy33auew2AgzajLLhQ1NyQIDAQABAoIBAGPeyyGU7jhbR/Q6
+                            JJU0JtSvVwkjdMHeKYAjXyhx31mXM4aUYU61j+GNDk3bXfJvmZiwkvJS/Fzg4gL0
+                            Y28ONtMLOrnhNo6QiJiw+NTr/WfQWoLQHm1DGYefL8rE2M1ti24VZlnrLleBzIWc
+                            qUaY0FmXY/b1UV3SFupQi+xUiWrcknrU+HrjbRoN+Pf1AOhyoDUOKII2umzWQP80
+                            40Pi9FhvVOIqOxO3P6lObA1hxPJ3IBFdq4sbXIMzDjQZJ2+V/4gaB7WQHdHfJzKy
+                            90aj3hkh34GorRCGKZm3oO4WBaNq4O0kLDXcVhG4JJGVOCaGXt//6YnnYQ17xjsB
+                            7kKyhi0CgYEA6UgwI9IEDcnlgToDn4KdH/X9nVcfRl/1OwNwqNCiejDyYSFBvqmx
+                            O7V+qdHeMG7/wB5HlVEpotrtVDOsJRU0cFaJRRO3cSbaMiFvxReQetzThJoMLTEn
+                            Qk/zf4jX+3PSwf8tZhno+lYx4cpTnFbVtBxpSpxXb9Q0UfRphs4T/wcCgYEA4p28
+                            Obc5nQxS1askeYQkElBkNkTQEAUcwBvJt4yofvFwIpM6nB5BNTirr5Fr6CtVuLnL
+                            hAwfwFkQ+SKUJ8xMssBmDakK+OaO6VsAptA17kon+b34F+Zzb2PK9zqAU8b9IyRn
+                            sQOse/PTQayrj08fMimiJk95CoM4IF+dE7sYSK8CgYEA2WDAIxDXYDXrW6DAsYmX
+                            eqnRmyUM6poOPOB2wYgwbuZfZW0FA3LCyV3VWs9KWR2FyfMtRxFrV+rjThwcZC2/
+                            QJMStMCrpo/KsP4BVcoggqx1mVGm2RG9gIhZ4VSV1iVWlhl/p22rcGKTds//W2Z7
+                            GquC3diTjYxS+K9lCg3MCk8CgYAImJ8pOVAQKwWBYWqSyXZs02vZuyaBVRBQIT23
+                            nrhNiG8OgLM6dNNYZ3vrvwJu3iX0Sef0bFlP/JDuQmeIUvGd3jqAzibCZkyJgZA2
+                            w7MoBNwbVx9wGKfMU+bBFhEEHIpBqQHd7xu93/Pli90+JGw0hOvHtEANLAkXAUl+
+                            FBzJtQKBgF01uWAaa/a9xxZglo05Rzq+bB/dnEx8faZhuhI1/aHMywZmiDabwyiG
+                            eC9ADYQrDOGSfhc4neGWaXcY6B2+je5cOAH2UvWqCwqBPOoFBsOWo37ZnVhXU6jE
+                            ZzxZyPwbkbMttd3BMFncIGFpq0nFQRduy/cbQY7Oq6aZoK6IJNA0
+                            -----END RSA PRIVATE KEY-----`;
+
+ const operatorPublicKey = `------BEGIN RSA PUBLIC KEY-----
+                            MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzoF3SsDDgXZB8+KjfTDs
+                            LYHRsMCq4yodylPRN3Tx6uAEHhPzAT4oO3yJsAkDKBD2QOAui/1fWQKl+w9jyWFu
+                            gumFF+pbciDmG2TFzfILrX/vGKG+WGQD/vCLQ1Oqf9AgZuAK+qxp1IfjLTNFk8Tm
+                            eNA8ub+pT5QAIetCnjMTqaG1FXv15B9ScCbefbCz49mqt2HQo8mjp91C2W0Bu4+l
+                            RByYN2wz3Z7GUx7NTqCH0yfyRwKGPYb2nuOFpqDM5BF/eZVmjhKz3Bmx7UKA70WK
+                            VSA/vD7VeEuxtZAsAA0aO2wKxyRg+tdQKo3JiqNlzOyZfy33auew2AgzajLLhQ1N
+                            yQIDAQAB
+                            -----END RSA PUBLIC KEY-----`;
+
+ export { operatorPrivateKey, operatorPublicKey };

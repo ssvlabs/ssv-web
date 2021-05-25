89ec20bbf416884c03a65fa63ceb0bbb0e1745c9
Dmitri Meshin <dmitri@blox.io>
2021-05-25 17:36:46 +0300

    Finished security policies

--- cypress/integration/ssv/add-operator.spec.js
+++ cypress/integration/ssv/add-operator.spec.js
@@ -2,31 +2,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { randomValueHex } from '~lib/utils/crypto';
 import config, { translations } from '~app/common/config';
+import { getRandomOperatorKey } from '~lib/utils/contract/operator';
 import testConfig from './config';
 
 config.CONTRACT.ADDRESS = testConfig.CONTRACT_ADDRESS;
 const operatorPublicKeyLength = config.FEATURE.OPERATORS.VALID_KEY_LENGTH;
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-const getRandomOperatorKey = (existing = false, corrupt = false) => {
-  const defaultExistingKey = 'LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJJakFOQmdrcWhraUc5dzBCQVFFRkFBT0NBUThBTUlJQkNnS0NBUUVBb3dFN09FYnd5TGt2clowVFU0amoKb295SUZ4TnZnclk4RmorV3NseVpUbHlqOFVEZkZyWWg1VW4ydTRZTWRBZStjUGYxWEsrQS9QOVhYN09CNG5mMQpPb0dWQjZ3ckMvamhMYnZPSDY1MHJ5VVlvcGVZaGxTWHhHbkQ0dmN2VHZjcUxMQit1ZTIvaXlTeFFMcFpSLzZWCnNUM2ZGckVvbnpGVHFuRkN3Q0YyOGlQbkpWQmpYNlQvSGNUSjU1SURrYnRvdGFyVTZjd3dOT0huSGt6V3J2N2kKdHlQa1I0R2UxMWhtVkc5UWpST3Q1NmVoWGZGc0ZvNU1xU3ZxcFlwbFhrSS96VU5tOGovbHFFZFUwUlhVcjQxTAoyaHlLWS9wVmpzZ21lVHNONy9acUFDa0h5ZTlGYmtWOVYvVmJUaDdoV1ZMVHFHU2g3QlkvRDdnd093ZnVLaXEyClR3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K';
-  if (existing) {
-    return defaultExistingKey;
-  }
-  const randomPosition = getRandomArbitrary(5, defaultExistingKey.length - 5);
-  let newKey = '';
-  let newChar = '1';
-  if (corrupt) {
-    newChar = '123';
-  }
-  newKey = `${defaultExistingKey.substr(0, randomPosition)}${newChar}${defaultExistingKey.substr(randomPosition + 1)}`;
-  return newKey.toString();
-};
 
 context('Add Operator', () => {
   before(() => {

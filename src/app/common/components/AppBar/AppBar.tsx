35d116aaca723f5fdc9e4b488be849bfce7875c6
Dmitri Meshin <dmitri@blox.io>
2021-08-01 14:08:53 +0300

    BLOXSSV-408: Correct ssv.network link

--- src/app/common/components/AppBar/AppBar.tsx
+++ src/app/common/components/AppBar/AppBar.tsx
@@ -17,114 +17,130 @@ import ContractOperator from '~app/common/stores/contract/ContractOperator.store
 import ConnectWalletButton from '~app/common/components/AppBar/components/ConnectWalletButton';
 
 type Button = {
-    text: string,
-    link: string,
-    newPage?: boolean
-    active?: boolean
+  text: string,
+  link: string,
+  newPage?: boolean
+  active?: boolean
 };
 
 const AppBarComponent = () => {
-    const classes = useStyles();
-    const stores = useStores();
-    const operatorStore: ContractOperator = stores.ContractOperator;
-    const { history } = useUserFlow();
-    const applicationStore: ApplicationStore = stores.Application;
+  const classes = useStyles();
+  const stores = useStores();
+  const operatorStore: ContractOperator = stores.ContractOperator;
+  const { history } = useUserFlow();
+  const applicationStore: ApplicationStore = stores.Application;
 
-    const buttons: Button[] = [
-        { text: 'join', link: config.routes.HOME, active: true },
-        { text: 'my account', link: config.routes.HOME, active: false },
-        { text: 'explorer', link: config.links.LINK_EXPLORER, newPage: true, active: true },
-        { text: 'docs', link: 'https://docs.ssv.network/', newPage: true, active: true },
-    ];
-    const menuButtons: Button[] = [
-        { text: 'Join the Network', link: config.routes.HOME, active: true },
-        { text: 'Documentation', link: 'https://docs.ssv.network/', newPage: true, active: true },
-        { text: 'explorer', link: config.links.LINK_EXPLORER, newPage: true, active: true },
-    ];
+  const buttons: Button[] = [
+    { text: 'join', link: config.routes.HOME, active: true },
+    { text: 'my account', link: config.routes.HOME, active: false },
+    { text: 'explorer', link: config.links.LINK_EXPLORER, newPage: true, active: true },
+    { text: 'docs', link: 'https://docs.ssv.network/', newPage: true, active: true },
+  ];
+  const menuButtons: Button[] = [
+    { text: 'Join the Network', link: config.routes.HOME, active: true },
+    { text: 'Documentation', link: 'https://docs.ssv.network/', newPage: true, active: true },
+    { text: 'explorer', link: config.links.LINK_EXPLORER, newPage: true, active: true },
+  ];
 
-    const renderMenu = () => {
-        return (
-          <div className={classes.fullScreen}>
-            <Grid container justify="center">
-              {menuButtons.map((button: Button, index: number) => {
-                  return (
-                    <Grid key={index} item xs={12} className={classes.menuButtonWrapper}>
-                      <Typography
-                        onClick={() => { if (button.active) { switchPage(button.link, button.newPage); } }}
-                        className={classes.menuButton}>
-                        {button.text}
-                      </Typography>
-                    </Grid>
-                  );
-              })}
-            </Grid>
-          </div>
-        );
-    };
-
-    const switchPage = (link: string, newPage: boolean = false) => {
-        if (newPage) {
-            window.open(link, '_blank');
-        } else {
-            operatorStore.clearOperatorData();
-            history.push(link);
-        }
-        applicationStore.displayToolBarMenu(false);
-    };
-
-    const handleClick = (status: boolean) => {
-        applicationStore.displayToolBarMenu(status);
-    };
-
+  const renderMenu = () => {
     return (
-      <div className={classes.root}>
-        <AppBar className={classes.bloxColor} position="static">
-          <Toolbar className={classes.toolbar}>
-            <Grid container justify={'space-between'} alignItems={'center'}>
-              <Grid item xs={4} md={4}>
-                <Typography onClick={() => { switchPage(config.routes.HOME); }}
-                  variant={'subtitle1'}
-                  className={classes.mainButton}>
-                  ssv.network
+      <div className={classes.fullScreen}>
+        <Grid container justify="center">
+          {menuButtons.map((button: Button, index: number) => {
+            return (
+              <Grid key={index} item xs={12} className={classes.menuButtonWrapper}>
+                <Typography
+                  onClick={() => {
+                    if (button.active) {
+                      switchPage(button.link, button.newPage);
+                    }
+                  }}
+                  className={classes.menuButton}>
+                  {button.text}
                 </Typography>
               </Grid>
-              <Grid item zeroMinWidth md={3}>
+            );
+          })}
+        </Grid>
+      </div>
+    );
+  };
+
+  const switchPage = (link: string, newPage: boolean = false) => {
+    if (isUpgradePage()) {
+      window.location.href = '/';
+      return;
+    }
+    if (newPage) {
+      window.open(link, '_blank');
+    } else {
+      operatorStore.clearOperatorData();
+      history.push(link);
+    }
+    applicationStore.displayToolBarMenu(false);
+  };
+
+  const handleClick = (status: boolean) => {
+    applicationStore.displayToolBarMenu(status);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.bloxColor} position="static">
+        <Toolbar className={classes.toolbar}>
+          <Grid container justify={'space-between'} alignItems={'center'}>
+            <Grid item xs={4} md={4}>
+              <Typography onClick={() => {
+                switchPage(config.routes.HOME);
+              }}
+                variant={'subtitle1'}
+                className={classes.mainButton}>
+                ssv.network
+              </Typography>
+            </Grid>
+            <Grid item zeroMinWidth md={3}>
+              {!isUpgradePage() ? (
+                <Grid container justify={'space-around'}>
+                  {buttons.map((button: Button, index: number) => {
+                    return (
+                      <Grid item key={index}>
+                        <Tooltip disableHoverListener={button.active} key={index} disableFocusListener
+                          disableTouchListener title="Comming soon...">
+                          <Typography onClick={() => {
+                            if (button.active) switchPage(button.link, button.newPage);
+                          }}
+                            variant={'subtitle1'}
+                            className={classes.button}>
+                            {button.text}
+                          </Typography>
+                        </Tooltip>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              ) : ''}
+            </Grid>
+            <Grid item xs={8} md={4}>
+              <Grid container spacing={1} alignItems={'center'} justify={'flex-end'}>
+                <Grid item>
+                  <ConnectWalletButton />
+                </Grid>
                 {!isUpgradePage() ? (
-                  <Grid container justify={'space-around'}>
-                    {buttons.map((button: Button, index: number) => {
-                      return (
-                        <Grid item key={index}>
-                          <Tooltip disableHoverListener={button.active} key={index} disableFocusListener disableTouchListener title="Comming soon...">
-                            <Typography onClick={() => { if (button.active) switchPage(button.link, button.newPage); }}
-                              variant={'subtitle1'}
-                              className={classes.button}>
-                              {button.text}
-                            </Typography>
-                          </Tooltip>
-                        </Grid>
-                        );
-                    })}
+                  <Grid item className={classes.menu} onClick={() => {
+                    handleClick(!applicationStore.shouldDisplayToolBar);
+                  }}>
+                    {applicationStore.shouldDisplayToolBar ? <CloseIcon className={classes.menuIcon} /> :
+                    <MenuIcon className={classes.menuIcon} />}
                   </Grid>
                 ) : ''}
               </Grid>
-              <Grid item xs={8} md={4}>
-                <Grid container spacing={1} alignItems={'center'} justify={'flex-end'}>
-                  <Grid item>
-                    <ConnectWalletButton />
-                  </Grid>
-                  {!isUpgradePage() ? (
-                    <Grid item className={classes.menu} onClick={() => { handleClick(!applicationStore.shouldDisplayToolBar); }}>
-                      {applicationStore.shouldDisplayToolBar ? <CloseIcon className={classes.menuIcon} /> : <MenuIcon className={classes.menuIcon} />}
-                    </Grid>
-                  ) : ''}
-                </Grid>
-              </Grid>
             </Grid>
-          </Toolbar>
-        </AppBar>
-        {applicationStore.shouldDisplayToolBar && renderMenu()}
-      </div>
-    );
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      {applicationStore.shouldDisplayToolBar && renderMenu()}
+    </div>
+  );
 };
 
 export default observer(AppBarComponent);

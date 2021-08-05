c5877ea5dbc8a40ae04dceca12004aa8606830b0
Dmitri Meshin <dmitri@blox.io>
2021-08-05 17:59:26 +0300

    Upgrade Page in standalone branch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Main from '~app/components/Routes';
 import { useStores } from '~app/hooks/useStores';
 import AppBar from '~app/common/components/AppBar';
+import UpgradeHome from '~app/components/UpgradeHome';
 import BarMessage from '~app/common/components/BarMessage';
 import WalletStore from '~app/common/stores/Wallet/Wallet.store';
 
 declare global {
-    interface Window {
-        ethereum: any;
-        web3: any;
-    }
+  interface Window {
+    ethereum: any;
+    web3: any;
+  }
 }
 
 const App = () => {
-    const stores = useStores();
-    const walletStore: WalletStore = stores.Wallet;
-    React.useEffect(() => {
-        walletStore.checkConnection();
-    }, []);
+  const stores = useStores();
+  const walletStore: WalletStore = stores.Wallet;
+  React.useEffect(() => {
+    walletStore.checkConnection();
+  }, []);
+
   return (
     <>
       <BarMessage />
       <AppBar />
-      <Main />
+      <UpgradeHome />
       <CssBaseline />
     </>
   );
--- src/lib/utils/navigation.ts
+++ src/lib/utils/navigation.ts
@@ -1,8 +1,6 @@
-import config from '~app/common/config';
-
 /**
  * Checks if the page is upgrade from CDT to SSV token.
  */
 export const isUpgradePage = () => {
-  return window.location.pathname.startsWith(config.routes.UPGRADE);
+  return true;
 };

92afaa5091ce6939c68eb5f33a2c45059a19227e
Dmitri Meshin <dmitri@blox.io>
2021-08-08 18:41:12 +0300

    Navigation fix

--- src/lib/utils/navigation.ts
+++ src/lib/utils/navigation.ts
@@ -1,6 +1,8 @@
+import config from '~app/common/config';
+
 /**
  * Checks if the page is upgrade from CDT to SSV token.
  */
 export const isUpgradePage = () => {
-  return true;
+  return window.location.pathname.startsWith(config.routes.UPGRADE);
 };

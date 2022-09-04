374e739e2e96f98b0e086b4ff97ea8ced6c468ac
Dmitri Meshin <dmitri@blox.io>
2022-09-04 16:16:24 +0300

    IO1-1478: slashing warning page fixes

--- src/app/components/common/CheckBox/CheckBox.tsx
+++ src/app/components/common/CheckBox/CheckBox.tsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
-import { useStyles } from './CheckBox.styles';
 import { Grid } from '@material-ui/core';
+import { useStyles } from '~app/components/common/CheckBox/CheckBox.styles';
 
 type Props = {
-    text: any,
-    width?: number,
-    height?: number,
-    disable?: boolean,
-    isChecked?: boolean,
-    onClickCallBack?: any,
-    grayBackGround?: boolean,
+  text: any,
+  width?: number,
+  height?: number,
+  disable?: boolean,
+  isChecked?: boolean,
+  onClickCallBack?: any,
+  grayBackGround?: boolean,
 };
 
 const CheckBox = (props: Props) => {
-    const { width, height, text, disable, isChecked, onClickCallBack, grayBackGround } = props;
-    const [checked, setChecked] = useState(isChecked ?? false);
-    const classes = useStyles({ grayBackGround, checked, width, height });
+  const { width, height, text, disable, isChecked, onClickCallBack, grayBackGround } = props;
+  const [checked, setChecked] = useState(isChecked ?? false);
+  const classes = useStyles({ grayBackGround, checked, width, height });
 
-    const checkAction = () => {
-        if (disable) return;
-        setChecked(!checked);
-        onClickCallBack && onClickCallBack(!checked);
-    };
-    
-    return (
-      <Grid container className={classes.CheckBoxWrapper} onClick={checkAction}>
-        <Grid item>
-          <Grid className={classes.BoxWrapper} />
-        </Grid>
-        <Grid item className={classes.Text}>
-          <Grid>{text}</Grid>
-        </Grid>
+  const checkAction = () => {
+    if (disable) return;
+    setChecked(!checked);
+    onClickCallBack && onClickCallBack(!checked);
+  };
+
+  return (
+    <Grid container className={classes.CheckBoxWrapper} onClick={checkAction}>
+      <Grid item>
+        <Grid className={classes.BoxWrapper} />
       </Grid>
-    );
+      <Grid item className={classes.Text}>
+        <Grid>{text}</Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CheckBox;

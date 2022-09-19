6d5c4ff109d605f27e01b1289a1c6e87a49f5bcd
Dmitri Meshin <dmitri@blox.io>
2022-09-19 13:22:51 +0300

    Maximum fee increase fix

--- src/lib/utils/validatesInputs.ts
+++ src/lib/utils/validatesInputs.ts
@@ -8,7 +8,7 @@ interface ErrorObject {
     errorMessage: string
 }
 
-export const validatePublicKeyInput = (value: string, callback: React.Dispatch<ErrorObject>) :void => {
+export const validatePublicKeyInput = (value: string, callback: React.Dispatch<ErrorObject>): void => {
     const response: ErrorObject = { shouldDisplay: true, errorMessage: '' };
     const regx = /^[A-Za-z0-9]+$/;
     if (value.length === 0) {
@@ -23,7 +23,7 @@ export const validatePublicKeyInput = (value: string, callback: React.Dispatch<E
     callback(response);
 };
 
-export const validateDisplayNameInput = (value: string, callback: React.Dispatch<ErrorObject>) :void => {
+export const validateDisplayNameInput = (value: string, callback: React.Dispatch<ErrorObject>): void => {
     const response = { shouldDisplay: true, errorMessage: '' };
     const regx = /^[A-Za-z0-9]+$/;
     if (value.length === 0) {
@@ -38,7 +38,7 @@ export const validateDisplayNameInput = (value: string, callback: React.Dispatch
     callback(response);
 };
 
-export const validateAddressInput = (value: string, callback: React.Dispatch<ErrorObject>) :void => {
+export const validateAddressInput = (value: string, callback: React.Dispatch<ErrorObject>): void => {
     const walletStore: WalletStore = WalletStore.getInstance().getStore('Wallet');
     const response = { shouldDisplay: true, errorMessage: '' };
     const regx = /^[A-Za-z0-9]+$/;
@@ -54,7 +54,7 @@ export const validateAddressInput = (value: string, callback: React.Dispatch<Err
     callback(response);
 };
 
-export const validateFeeInput = (value: string, callback: any) :void => {
+export const validateFeeInput = (value: string, callback: any): void => {
     // const walletStore: WalletStore = WalletStore.getInstance().getStore('Wallet');
     const response = { shouldDisplay: false, errorMessage: '' };
     // eslint-disable-next-line radix
@@ -71,18 +71,18 @@ export const validateFeeInput = (value: string, callback: any) :void => {
     callback(response);
 };
 
-export const validateFeeUpdate = (previousValue: number, newValue: string, maxFeeIncrease: number, callback: any) :void => {
+export const validateFeeUpdate = (previousValue: number, newValue: string, maxFeeIncrease: number, callback: any): void => {
     const response = { shouldDisplay: false, errorMessage: '' };
+    const feeMaximumIncrease = new Decimal(previousValue).mul(maxFeeIncrease).dividedBy(100).plus(previousValue - 0.01);
     if (Number.isNaN(Number(newValue)) || Number.isFinite(newValue) || !newValue) {
         response.shouldDisplay = true;
         response.errorMessage = 'Please use numbers only.';
     } else if (compareNumbers(previousValue, newValue)) {
         response.shouldDisplay = true;
         response.errorMessage = 'Please set a different fee amount from current.';
-    }
-    else if (new Decimal(previousValue).mul(maxFeeIncrease).dividedBy(100).plus(previousValue).lessThan(newValue)) {
+    } else if (feeMaximumIncrease.lessThan(newValue)) {
         response.shouldDisplay = true;
-        response.errorMessage = `You can only increase your fee up to ${new Decimal(previousValue).mul(maxFeeIncrease).dividedBy(100).plus(previousValue - 0.01).toFixed().toString()}`;
+        response.errorMessage = `You can only increase your fee up to ${feeMaximumIncrease.toFixed().toString()}`;
     }
     // eslint-disable-next-line radix
     else if (new Decimal(newValue).dividedBy(config.GLOBAL_VARIABLE.BLOCKS_PER_YEAR).lessThan(config.GLOBAL_VARIABLE.MINIMUM_OPERATOR_FEE_PER_BLOCK)) {

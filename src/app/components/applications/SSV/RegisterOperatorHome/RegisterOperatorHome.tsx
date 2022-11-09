e333b83ce644a3c28125e031aae1ef4f4beb1e33
guy muroch <guym@blox.io>
2022-11-09 15:35:06 +0200

    fix tests routes

--- src/app/components/applications/SSV/RegisterOperatorHome/RegisterOperatorHome.tsx
+++ src/app/components/applications/SSV/RegisterOperatorHome/RegisterOperatorHome.tsx
@@ -19,18 +19,18 @@ const RegisterOperatorHome = () => {
         <Grid container>
           <HeaderSubHeader title={'Join the SSV Network Operators'}
             subtitle={'To join the network of operators you must run an SSV node. Setup your node, generate operator keys and register to the network.'}
-          />
+              />
           <Grid container item justify={'space-evenly'}>
             <Grid container item className={classes.LinkButtonWrapper}>
               <Grid item xs={12}>
                 <SecondaryButton text={'Run SSV Node'} submitFunction={() => {
-                  GoogleTagManager.getInstance().sendEvent({
-                    category: 'external_link',
-                    action: 'click',
-                    label: 'Run SSV Node',
-                  });
-                  window.open('https://docs.ssv.network/run-a-node/operator-node/installation');
-                }} />
+                      GoogleTagManager.getInstance().sendEvent({
+                        category: 'external_link',
+                        action: 'click',
+                        label: 'Run SSV Node',
+                      });
+                      window.open('https://docs.ssv.network/run-a-node/operator-node/installation');
+                    }} />
               </Grid>
               <Grid item xs={12} className={classes.UnderButtonText}>
                 See our developer documentation
@@ -39,8 +39,8 @@ const RegisterOperatorHome = () => {
             <Grid container item className={classes.LinkButtonWrapper}>
               <Grid item xs={12}>
                 <SecondaryButton text={'Register Operator'} submitFunction={() => {
-                  history.push(config.routes.SSV.OPERATOR.GENERATE_KEYS);
-                }} />
+                      history.push(config.routes.SSV.OPERATOR.GENERATE_KEYS);
+                    }} />
               </Grid>
               <Grid item xs={12} className={classes.UnderButtonText}>
                 Sign up as one of the network operators
@@ -48,8 +48,8 @@ const RegisterOperatorHome = () => {
             </Grid>
           </Grid>
         </Grid>,
-      ]}
-    />
+          ]}
+      />
   );
 };
 

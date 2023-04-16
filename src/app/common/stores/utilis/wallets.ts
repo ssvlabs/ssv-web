34dd9313da1c26d14b38bff56fdafd3362d63c2a
Dima <axelrod@blox.io>
2023-04-16 18:56:07 +0300

    IO1-3045: added wallet connect support

--- src/app/common/stores/utilis/wallets.ts
+++ src/app/common/stores/utilis/wallets.ts
@@ -1,7 +1,5 @@
 export const wallets: any = [
     { walletName: 'metamask' },
+    { walletName: 'walletConnect', infuraKey: '68cc79febad74019825762c8384ddbeb' },
 ];
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    // true for mobile device
-    wallets.push({ walletName: 'walletConnect', infuraKey: '68cc79febad74019825762c8384ddbeb' });
-}
+

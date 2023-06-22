58b38ded27c33aec674098419a111c1bd3d70ac9
Dima Axelrod <axerod@blox.io>
2023-06-22 13:19:40 +0300

    changed function name

--- src/lib/utils/gasPriceHelper.ts
+++ src/lib/utils/gasPriceHelper.ts
@@ -1,6 +1,6 @@
 import { GasGroup, MAX_GAS_PER_GROUP } from '~app/common/config/gasPrices';
 
-const addTenPercent = (value: number): number =>  {
+const calculateTenPercentIncrease = (value: number): number =>  {
     return Math.floor(value * 1.2);
 };
 
@@ -8,12 +8,12 @@ export const getRegisterValidatorGasPrice = (clusterExist: boolean, countOfOpera
     const EVENT = 'REGISTER_VALIDATOR';
     const EVENT_TYPE = clusterExist ? withoutDeposit ? 'WITHOUT_DEPOSIT' : 'EXISTING_CLUSTER' : 'NEW_STATE';
     const OPERATORS_COUNT = countOfOperators > 4 ? `_${countOfOperators}` : '';
-    return addTenPercent(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}_${EVENT_TYPE}${OPERATORS_COUNT}`]]);
+    return calculateTenPercentIncrease(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}_${EVENT_TYPE}${OPERATORS_COUNT}`]]);
 };
 
 export const getLiquidationGasPrice = (countOfOperators: number): number => {
     const EVENT = 'LIQUIDATE_CLUSTER';
-    return addTenPercent(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}${countOfOperators}`]]);
+    return calculateTenPercentIncrease(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}${countOfOperators}`]]);
 };
 
-export const getFixedGasPrice = (eventType: number) => addTenPercent(MAX_GAS_PER_GROUP[eventType]);
+export const getFixedGasPrice = (eventType: number) => calculateTenPercentIncrease(MAX_GAS_PER_GROUP[eventType]);

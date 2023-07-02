6039d60d418f4126ae138896b54b27a54a470f83
Dima Axelrod <axerod@blox.io>
2023-07-02 12:03:59 +0300

    IO1-3700: added flag for use hardcoded gas limit

--- src/lib/utils/gasLimitHelper.ts
+++ src/lib/utils/gasLimitHelper.ts
@@ -1,17 +1,23 @@
 import { GasGroup, MAX_GAS_PER_GROUP } from '~app/common/config/gasLimits';
 
-const calculateTenPercentIncrease = (value: number): number =>  {
-    return Math.floor(value * 1.2);
+const USE_HARDCODED_GAS_LIMIT = 'USE_HARDCODED_GAS_LIMIT';
+
+const calculateTenPercentIncrease = (value: number): number | null =>  {
+      const result = window.localStorage.getItem(USE_HARDCODED_GAS_LIMIT);
+      if ( result && JSON.parse(result)) {
+          return Math.floor(value * 1.2);
+      }
+    return null;
 };
 
-export const getRegisterValidatorGasLimit = (clusterExist: boolean, countOfOperators: number, withoutDeposit: boolean): number => {
+export const getRegisterValidatorGasLimit = (clusterExist: boolean, countOfOperators: number, withoutDeposit: boolean): number | null => {
     const EVENT = 'REGISTER_VALIDATOR';
     const EVENT_TYPE = clusterExist ? withoutDeposit ? 'WITHOUT_DEPOSIT' : 'EXISTING_CLUSTER' : 'NEW_STATE';
     const OPERATORS_COUNT = countOfOperators > 4 ? `_${countOfOperators}` : '';
     return calculateTenPercentIncrease(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}_${EVENT_TYPE}${OPERATORS_COUNT}`]]);
 };
 
-export const getLiquidationGasLimit = (countOfOperators: number): number => {
+export const getLiquidationGasLimit = (countOfOperators: number): number | null => {
     const EVENT = 'LIQUIDATE_CLUSTER';
     return calculateTenPercentIncrease(MAX_GAS_PER_GROUP[GasGroup[`${EVENT}_${countOfOperators}`]]);
 };

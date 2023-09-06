56f9ccdac565e57e8242692bf42bd5bf45838320
yoanatanblox <94914141+yoanatanblox@users.noreply.github.com>
2023-09-06 16:36:50 +0300

    bloxroute-ethical-deprication

--- src/lib/utils/operatorMetadataHelper.ts
+++ src/lib/utils/operatorMetadataHelper.ts
@@ -98,7 +98,7 @@ export const FIELDS: { [key: string]: MetadataEntity } = {
         value: '',
         errorMessage: '',
         placeholderText: 'Aestus, Agnostic Gnosis, Blocknative...',
-        options: ['Aestus', 'Agnostic Gnosis', 'Blocknative', 'bloXroute Ethical', 'bloXroute Max Profit', 'bloXroute Regulated', 'Eden Network', 'Flashbots', 'Manifold', 'Ultra Sound'],
+        options: ['Aestus', 'Agnostic Gnosis', 'Blocknative', 'bloXroute Max Profit', 'bloXroute Regulated', 'Eden Network', 'Flashbots', 'Manifold', 'Ultra Sound'],
     },
     [FIELD_KEYS.LOCATION]: {
         label: 'Server Geolocation',

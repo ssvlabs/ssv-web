40a2378b4012e342ae8627d1ed84676fe1bdacc8
Dima Axelrod <axerod@blox.io>
2023-09-12 10:23:09 +0300

    added new app bar button with links

---
+++ src/app/components/common/AppLinksToggle/AppLinkOption.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
+import { useStyles } from '~app/components/common/AppLinksToggle/AppLinksToggle.styles';
+
+const AppLinkOption = ({ optionLabel, link, bottomLine }: { optionLabel: string, link: string, bottomLine: boolean }) => {
+    const classes = useStyles({ bottomLine });
+
+    const onClickHandler = () => {
+        window.open(link, '_blank');
+    };
+
+    return (
+        <Grid container item className={classes.Button} onClick={onClickHandler}>
+            <Typography className={classes.OptionLabel}>{optionLabel}</Typography>
+            <Grid className={classes.Icon}/>
+        </Grid>
+    );
+};
+
+export default AppLinkOption;
---
+++ src/app/components/common/AppLinksToggle/AppLinksToggle.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Grid from '@mui/material/Grid';
+import AppLinkOption from './AppLinkOption';
+import { useStyles } from '~app/components/common/AppLinksToggle/AppLinksToggle.styles';
+import SocialNetworkLinks from '~app/components/common/AppLinksToggle/SocialNetworkLinks';
+
+type ToggleOptionModel = {
+    label: string,
+    link: string,
+    bottomLine: boolean
+};
+
+type ToggleSelectProps = {
+    label: string,
+    options: ToggleOptionModel[]
+};
+
+const AppLinksToggle = ({ label, options }: ToggleSelectProps) => {
+    const classes = useStyles({});
+    const optionsRef = useRef(null);
+    const [showOptions, setShowOptions] = useState(false);
+
+    useEffect(() => {
+        const handleClickOutside = () => {
+            setTimeout(() => {
+                if (showOptions) {
+                setShowOptions(false);
+            }}, 200);
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [optionsRef, showOptions]);
+
+    const switchShowOptions = () => {
+        showOptions ? setShowOptions(false) : setShowOptions(true);
+    };
+
+    return (
+        <Grid className={classes.ToggleWrapper} onClick={switchShowOptions}>
+            <Grid className={classes.DotsGroupWrapper}>
+              {label}
+            </Grid>
+            {showOptions && <Grid item className={classes.OptionsWrapper}>
+                <Grid ref={optionsRef}  container item className={classes.Options}>
+                    {options.map((option:ToggleOptionModel) => <AppLinkOption bottomLine={option.bottomLine} key={option.label} optionLabel={option.label} link={option.link}/>) }
+                    <SocialNetworkLinks />
+                </Grid>
+            </Grid>}
+        </Grid>
+    );
+};
+
+export default AppLinksToggle;

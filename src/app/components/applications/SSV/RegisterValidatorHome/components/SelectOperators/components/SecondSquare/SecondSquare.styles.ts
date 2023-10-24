4872dccd60c183a894ad0687d4b398a60e8a3c08
Dima Axelrod <axerod@blox.io>
2023-10-24 18:33:10 +0600

    fixed small UI bug on choose operators

--- src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/SecondSquare/SecondSquare.styles.ts
+++ src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/SecondSquare/SecondSquare.styles.ts
@@ -38,9 +38,10 @@ export const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     maxHeight: 460,
     overflowY: 'scroll',
+    paddingTop: 10,
   },
   SelectedOperatorBox: {
-    width: 360,
+    width: '97%',
     height: 97,
     fontSize: 16,
     fontWeight: 500,
@@ -54,7 +55,7 @@ export const useStyles = makeStyles((theme: Theme) => ({
     backgroundColor: theme.colors.white,
     border: `solid 1px ${theme.colors.gray20}`,
     '@media only screen and (max-width: 1400px)': {
-      width: '100%',
+      width: '98%',
     },
   },
 

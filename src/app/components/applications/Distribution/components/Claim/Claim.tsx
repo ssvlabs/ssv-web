b6e2b70dfd1b3861ccf20da34663a5d5eef92874
Dima Axelrod <axerod@blox.io>
2023-11-16 01:04:47 +0600

    adapted claim app for goerli and mainnet contracts, added helper to test contract

--- src/app/components/applications/Distribution/components/Claim/Claim.tsx
+++ src/app/components/applications/Distribution/components/Claim/Claim.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import { useStores } from '~app/hooks/useStores';
+import { getCurrentNetwork } from '~lib/utils/envHelper';
+import { useDistributionStore } from '~app/hooks/useDistributionStore';
+import WalletStore from '~app/common/stores/applications/Distribution/Wallet.store';
 import DistributionStore from '~app/common/stores/applications/Distribution/Distribution.store';
 import EligibleScreen from '~app/components/applications/Distribution/components/Claim/EligibleScreen';
 import NotEligibleScreen from '~app/components/applications/Distribution/components/Claim/NotEligibleScreen';
-import WalletStore from '~app/common/stores/applications/Distribution/Wallet.store';
+import DistributionTestnetStore from '~app/common/stores/applications/Distribution/DistributionTestnet.store';
 
 const CLAIM_FLOW = {
   NOT_ELIGIBLE: 0,
@@ -13,9 +16,12 @@ const CLAIM_FLOW = {
 
 const Claim = () => {
   const stores = useStores();
-  const distributionStore: DistributionStore = stores.Distribution;
+  const { networkId } = getCurrentNetwork();
+  const distributionStore: DistributionStore | DistributionTestnetStore = useDistributionStore(networkId);
   const walletStore: WalletStore = stores.Wallet;
   const [currentClaimFlow, setCurrentClaimFlow] = useState(distributionStore.userAddress ? CLAIM_FLOW.ELIGIBLE : CLAIM_FLOW.NOT_ELIGIBLE);
+
+  console.log(distributionStore);
 
   useEffect(() => {
     setCurrentClaimFlow(distributionStore.userAddress ? CLAIM_FLOW.ELIGIBLE : CLAIM_FLOW.NOT_ELIGIBLE);
@@ -25,7 +31,6 @@ const Claim = () => {
     [CLAIM_FLOW.ELIGIBLE]: EligibleScreen,
     [CLAIM_FLOW.NOT_ELIGIBLE]: NotEligibleScreen,
   };
-
 
   const Component = components[currentClaimFlow];
 

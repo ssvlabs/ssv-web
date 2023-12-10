0ca4a62ce51c08af149b8ba7c2749b5ee0f47768
Dima Axelrod <axerod@blox.io>
2023-12-10 17:25:50 +0600

    removed duplicate unnecessary function

--- src/lib/utils/envHelper.ts
+++ src/lib/utils/envHelper.ts
@@ -222,21 +222,6 @@ export const changeCurrentNetwork = (networkId: number, version?: string) => {
   }
 };
 
-
-export const getCurrentNetwork = () => {
-  if (!NETWORKS_DATA && !process.env.REACT_APP_DISABLE_NETWORK_DATA_CHECK) throw new Error('Provide network data');
-  const currentNetworkIndex = window.localStorage.getItem('current_network');
-  if (currentNetworkIndex && NETWORKS_DATA.length > 1) {
-    const networkId = NETWORKS_DATA[currentNetworkIndex].networkId;
-    return { ...NETWORKS_DATA[currentNetworkIndex], ...NETWORK_VARIABLES[networkId] };
-  }
-  if (!currentNetworkIndex && process.env.REACT_APP_FAUCET_PAGE) {
-    const holeskyIndex = NETWORKS_DATA.findIndex((networkData: any) => networkData.networkId === NETWORKS.HOLESKY);
-    return saveNetwork(holeskyIndex);
-  }
-  return saveNetwork(FIRST_NETWORK_INDEX);
-};
-
 export const currentNetworkName = () => NETWORK_NAMES[getCurrentNetwork().networkId];
 
 export const isMainnet = getCurrentNetwork().networkId === NETWORKS.MAINNET;

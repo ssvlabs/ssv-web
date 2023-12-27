ec1f55a890c01965b4fe8ec529048f1fac2c5c57
Alex Stoliar <alexstolr@gmail.com>
2023-12-27 12:10:51 +0200

    fix merge issues

--- src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/ClusterSize/ClusterSize.tsx
+++ src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/ClusterSize/ClusterSize.tsx
@@ -26,7 +26,6 @@ const ClusterSize = ({ currentClusterSize, changeClusterSize }: { currentCluster
         <Grid className={classes.ClusterSizeWrapper}>
             <Typography className={classes.Title}>Cluster Size</Typography>
             {sizes.map((size: number, index: number) => <Grid key={`${size}${index}`} onClick={() => clusterSizeHandler(size)} className={`${classes.ClusterSizeButton} ${size === currentClusterSize && classes.ChosenClusterSize}`}>{size}</Grid>)}
-            /*{sizes.map((size: number, index: number) => <Grid key={index} onClick={() => clusterSizeHandler(size)} className={`${classes.ClusterSizeButton} ${size === currentClusterSize && classes.ChosenClusterSize}`}>{size}</Grid>)}*/
         </Grid>
     );
 };

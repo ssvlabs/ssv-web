9bdb70e90650abed972ce6f7eab7ad0c05b942d2
Ilya <ilya@blox.io>
2024-02-21 14:16:59 +0200

    cleanup

--- src/app/hooks/useWalletDisconnector.ts
+++ src/app/hooks/useWalletDisconnector.ts
@@ -1,5 +1,4 @@
-import { useConnectWallet, useSetChain } from '@web3-onboard/react';
-import { useEffect } from 'react';
+import { useConnectWallet } from '@web3-onboard/react';
 import { cleanLocalStorageAndCookie } from '~lib/utils/onboardHelper';
 import config from '~app/common/config';
 import { useNavigate } from 'react-router-dom';
@@ -8,22 +7,10 @@ import WalletStore from '~app/common/stores/Abstracts/Wallet';
 import { resetContracts } from '~root/services/contracts.service';
 
 const useWalletDisconnector = () => {
-  const [{ wallet }, connect, disconnect] = useConnectWallet();
-  const [{ connectedChain }] = useSetChain();
+  const [_, __, disconnect] = useConnectWallet();
   const navigate = useNavigate();
   const stores = useStores();
   const walletStore: WalletStore = stores.Wallet;
-
-  // useEffect(() => {
-  //   if (walletStore.wallet) {
-  //     cleanLocalStorageAndCookie();
-  //     disconnect({ label: walletStore.wallet.label });
-  //     walletStore.initWallet(null, null);
-  //     navigate(config.routes.SSV.ROOT);
-  //   } else {
-  //     walletStore.initWallet(wallet, connectedChain);
-  //   }
-  // }, [walletStore.wallet]);
 
   const disconnectWallet = async () => {
     cleanLocalStorageAndCookie();

42da241343ada01e80cccc609f68e18b49fec5d3
Ilya <ilya@blox.io>
2024-03-04 14:47:50 +0200

    CR fixes

--- src/app/components/applications/SSV/TransactionPendingPopUp/TransactionPendingPopUp.tsx
+++ src/app/components/applications/SSV/TransactionPendingPopUp/TransactionPendingPopUp.tsx
@@ -13,8 +13,29 @@ import HeaderSubHeader from '~app/components/common/HeaderSubHeader';
 import { WalletStore } from '~app/common/stores/applications/SsvWeb';
 import { getEtherScanLink } from '~root/providers/networkInfo.provider';
 import AddressKeyInput from '~app/components/common/AddressKeyInput/AddressKeyInput';
-import { useStyles } from '~app/components/applications/SSV/TransactionPendingPopUp/TransactionPendingPopUp.styles';
 import { getIsShowTxPendingPopup, getTxHash, setIsLoading, setIsShowTxPendingPopup } from '~app/redux/appState.slice';
+
+const DialogWrapper = styled(Dialog)<{ theme: any }>`
+    & > div > div {
+        border-radius: 16px;
+        background-color: ${({ theme }) => theme.colors.white};
+    }
+`;
+
+const GridWrapper = styled(Grid)<{ theme: any }>`
+    width: 424px;
+    padding: 32px;
+    border-radius: 16px;
+    align-items: center;
+    flex-direction: column;
+    background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const ImageWrapper = styled.img<{ hasMarginBottom: boolean }>`
+    width: 100px;
+    margin-top: 20px;
+    margin-bottom: ${({ hasMarginBottom }) => hasMarginBottom ? 40 : 0}px;
+`;
 
 const AdditionText = styled.div`
     width: 100%;
@@ -23,17 +44,21 @@ const AdditionText = styled.div`
     color: ${({ theme }) => theme.colors.gray80}
 `;
 
-enum PopUpTypes {
-  DEFAULT = 'default',
-  MULTI_SIGN = 'multi',
-}
+const ValidatorText = styled.div<{ theme: any }>`
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1.14;
+    text-align: left;
+    color: ${({ theme }) => theme.colors.gray40};
+    margin-bottom: 8px;
+`;
 
 const POP_UP_DATA = {
-  [PopUpTypes.DEFAULT]: {
+  'false': {
     title: 'Sending Transaction',
     subTitles: 'Your transaction is pending on the blockchain - please wait while it\'s being confirmed',
   },
-  [PopUpTypes.MULTI_SIGN]: {
+  'true': {
     title: 'Transaction Initiated',
     subTitles: 'Your transaction has been successfully initiated within the multi-sig wallet and is now pending approval from other participants.',
   },
@@ -46,9 +71,6 @@ const TransactionPendingPopUp = () => {
   const navigate = useNavigate();
   const isShowTxPendingPopup = useAppSelector(getIsShowTxPendingPopup);
   const txHash = useAppSelector(getTxHash);
-  const popUpType = walletStore.isContractWallet ? PopUpTypes.MULTI_SIGN : PopUpTypes.DEFAULT;
-  const isMultiSign = popUpType === PopUpTypes.MULTI_SIGN;
-  const classes = useStyles({ loaderBottom : isMultiSign ? 0 : 40 });
 
   const closeButtonAction = () => {
     dispatch(setIsLoading(false));
@@ -57,23 +79,23 @@ const TransactionPendingPopUp = () => {
   };
 
   return (
-    <Dialog className={classes.DialogWrapper} aria-labelledby="simple-dialog-title" open={isShowTxPendingPopup}>
-      <Grid className={classes.gridWrapper} container>
-        <HeaderSubHeader closeButtonAction={closeButtonAction} showCloseButton={isMultiSign} title={POP_UP_DATA[popUpType].title}
-                         subtitle={POP_UP_DATA[popUpType].subTitles}/>
-        {isMultiSign && <AdditionText>Please return to this web app once approved.</AdditionText>}
+    <DialogWrapper aria-labelledby="simple-dialog-title" open={isShowTxPendingPopup}>
+      <GridWrapper container>
+        <HeaderSubHeader closeButtonAction={closeButtonAction} showCloseButton={walletStore.isContractWallet} title={POP_UP_DATA[`${walletStore.isContractWallet}`].title}
+                         subtitle={POP_UP_DATA[`${walletStore.isContractWallet}`].subTitles}/>
+        {walletStore.isContractWallet && <AdditionText>Please return to this web app once approved.</AdditionText>}
         <Grid item>
-          <img className={classes.Loader} src={getImage('ssv-loader.svg')} alt="loader"/>
+          <ImageWrapper src={getImage('ssv-loader.svg')} alt="loader" hasMarginBottom={!walletStore.isContractWallet} />
         </Grid>
-        {!isMultiSign && <Grid item container style={{ marginBottom: 20 }}>
+        {!walletStore.isContractWallet && <Grid item container style={{ marginBottom: 20 }}>
           <Grid item xs>
-            <div className={classes.validatorText}>Transaction Hash</div>
+            <ValidatorText>Transaction Hash</ValidatorText>
           </Grid>
           <AddressKeyInput whiteBackgroundColor withCopy address={txHash}/>
         </Grid>}
-        {!isMultiSign && <LinkText text={'View on Etherscan'} link={`${getEtherScanLink()}/tx/${txHash}`}/>}
-      </Grid>
-    </Dialog>
+        {!walletStore.isContractWallet && <LinkText text={'View on Etherscan'} link={`${getEtherScanLink()}/tx/${txHash}`}/>}
+      </GridWrapper>
+    </DialogWrapper>
   );
 };
 

ed696587ec387c45208c117d643746773e74030b
Ilya <ilya@blox.io>
2024-03-13 15:36:50 +0200

    INTERFACES-244 fix switching network while connected to metamask

--- src/app/common/stores/applications/Faucet/Wallet.store.ts
+++ src/app/common/stores/applications/Faucet/Wallet.store.ts
@@ -8,7 +8,7 @@ import notifyService from '~root/services/notify.service';
 class WalletStore extends BaseStore implements Wallet {
   wallet: any = null;
   accountAddress: string = '';
-  isWalletConnect = false;
+  isNotMetamask = false;
 
   constructor() {
     super();

d928c9cda0a2a1437cec9b19327cfb63f5b4ef95
Ilya <ilya@blox.io>
2024-03-17 16:28:11 +0200

    INTERFACES-259 fix reload on change wallet

--- src/app/components/common/AppBar/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ src/app/components/common/AppBar/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -18,7 +18,7 @@ const ConnectWalletButton = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (walletStore.wallet && walletStore.wallet.accounts[0].address !== wallet?.accounts[0].address) {
+    if (walletStore.wallet && wallet && walletStore.wallet.accounts[0].address !== wallet.accounts[0].address) {
       window.location.reload();
     }
     if (wallet && connectedChain && !connecting && !walletStore.wallet) {

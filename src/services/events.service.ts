03b7194ad9ddf659ef8c13580a57fdcdd7753178
Ilya <ilya@blox.io>
2024-03-28 12:52:30 +0200

    small optimization

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -1,18 +1,33 @@
-import config from '~app/common/config';
+const CHECK_UPDATES_MAX_ITERATIONS = 60;
 
-export const executeAfterEvent = async (condition: Function, callBack: Function, delay: Function) => {
+const checkIfStateChanged = async (getter: any, valueBefore: any): Promise<boolean> => {
+  try {
+    const valueAfter = await getter();
+    return JSON.stringify(valueBefore) !== JSON.stringify(valueAfter);
+  } catch (e) {
+    console.error('checkIfStateChanged ', e);
+    return false;
+  }
+};
+
+const delay = async (ms?: number) => (new Promise((r) => setTimeout(() => r(true), ms || 1000)));
+
+export const executeAfterEvent = async ({ updatedStateGetter, prevState, callBack }: { updatedStateGetter: any; prevState?: any; callBack: Function }) => {
   let iterations = 0;
-  while (iterations <= config.GLOBAL_VARIABLE.CHECK_UPDATES_MAX_ITERATIONS) {
+  while (iterations <= CHECK_UPDATES_MAX_ITERATIONS) {
     iterations += 1;
-    const res = await condition();
+    let res;
+    if (prevState) {
+      res = await checkIfStateChanged(updatedStateGetter, prevState);
+    } else {
+      res = await updatedStateGetter();
+    }
     if (res) {
       await callBack();
-      iterations = config.GLOBAL_VARIABLE.CHECK_UPDATES_MAX_ITERATIONS + 1;
-      continue;
+      iterations = CHECK_UPDATES_MAX_ITERATIONS + 1;
     } else {
-      console.log('Transaction still not caught...');
+      await delay();
     }
-    await delay();
   }
   return true;
 };

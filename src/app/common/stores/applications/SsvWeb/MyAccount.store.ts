485605e8ffaeb0da0156bcc54f7b1a79f8bf5b24
axelrod-blox <128966996+axelrod-blox@users.noreply.github.com>
2024-04-17 12:39:16 +0300

    fixed bug with white screen, added update process cluster every time when we update clusters by owner address (#790)

--- src/app/common/stores/applications/SsvWeb/MyAccount.store.ts
+++ src/app/common/stores/applications/SsvWeb/MyAccount.store.ts
@@ -3,10 +3,11 @@ import BaseStore from '~app/common/stores/BaseStore';
 import { clustersByOwnerAddress } from '~root/services/validator.service';
 import { getOperatorsByOwnerAddress } from '~root/services/operator.service';
 import { extendClusterEntity } from '~root/services/cluster.service';
-import { SsvStore } from '~app/common/stores/applications/SsvWeb';
+import { ProcessStore, SsvStore } from '~app/common/stores/applications/SsvWeb';
 import { DEFAULT_PAGINATION } from '~app/common/config/config';
 import { IOperator } from '~app/model/operator.model';
 import { store } from '~app/store';
+import { SingleCluster } from '~app/model/processes.model';
 
 class MyAccountStore extends BaseStore {
   // OPERATOR
@@ -42,6 +43,8 @@ class MyAccountStore extends BaseStore {
 
   async getOwnerAddressClusters({ forcePage, forcePerPage }: { forcePage?: number, forcePerPage?: number }): Promise<any[]> {
     const ssvStore: SsvStore = this.getStore('SSV');
+    const processStore: ProcessStore = this.getStore('Process');
+    const process: SingleCluster = processStore?.getProcess;
     const accountAddress = store.getState().walletState.accountAddress;
     if (!accountAddress) return [];
     const { page, per_page } = this.ownerAddressClustersPagination;
@@ -51,6 +54,10 @@ class MyAccountStore extends BaseStore {
     this.ownerAddressClustersPagination = response.pagination;
     this.ownerAddressClusters = await Promise.all(response.clusters.map((cluster: any) => extendClusterEntity(cluster, accountAddress, ssvStore.liquidationCollateralPeriod, ssvStore.minimumLiquidationCollateral)));
     this.ownerAddressClusters = this.ownerAddressClusters.filter((cluster: any) => cluster.validatorCount > 0 || !cluster.isLiquidated);
+    if (process && process.processName === 'single_cluster') {
+      const updatedCluster = this.ownerAddressClusters.find((cluster: any) => cluster.id === process.item.id);
+      process.item = { ...process.item, ...updatedCluster };
+    }
     return this.ownerAddressClusters;
   }
 

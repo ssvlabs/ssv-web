b1dfa6c4865e3a73b2269ebbfe2f0554320eb287
Ilya <ilya@blox.io>
2024-05-12 12:55:20 +0300

    PR review fixes

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,8 +1,46 @@
 import { setIsLoading, setIsShowTxPendingPopup, setTxHash } from '~app/redux/appState.slice';
 import { setMessageAndSeverity } from '~app/redux/notifications.slice';
 import { translations } from '~app/common/config';
-import { executeAfterEvent } from '~root/services/events.service';
 import { refreshOperatorsAndClusters } from '~app/redux/account.slice';
+
+const CHECK_UPDATES_MAX_ITERATIONS = 60;
+
+const checkIfStateChanged = async (updatedStateGetter: any, prevState: any): Promise<boolean> => {
+  try {
+    const newState = await updatedStateGetter();
+    return JSON.stringify(prevState) !== JSON.stringify(newState);
+  } catch (e) {
+    console.error('checkIfStateChanged ', e);
+    return false;
+  }
+};
+
+const delay = async (ms?: number) => (new Promise((r) => setTimeout(() => r(true), ms || 1000)));
+
+export const executeAfterEvent = async ({ updatedStateGetter, prevState, callBack, txHash }: {
+  updatedStateGetter: any;
+  prevState?: any;
+  callBack: Function;
+  txHash?: string
+}) => {
+  let iterations = 0;
+  while (iterations <= CHECK_UPDATES_MAX_ITERATIONS) {
+    iterations += 1;
+    let res;
+    if (prevState) {
+      res = await checkIfStateChanged(updatedStateGetter, prevState);
+    } else {
+      res = await updatedStateGetter(txHash);
+    }
+    if (res) {
+      await callBack();
+      iterations = CHECK_UPDATES_MAX_ITERATIONS + 1;
+    } else {
+      await delay();
+    }
+  }
+  return true;
+};
 
 export const transactionExecutor = async ({ contractMethod, payload, isContractWallet, getterTransactionState, prevState, dispatch }: {
   contractMethod: Function,

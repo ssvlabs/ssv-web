b17e5e478bd6c09258295a44e8dc15b834797cbf
axelrod-blox <128966996+axelrod-blox@users.noreply.github.com>
2024-05-15 15:04:50 +0300

    added new condition for dkg validation (#844)
    * added new condition for dkg validation
    
    * required changes
    
    ---------
    
    Co-authored-by: Dima Axelrod <axerod@blox.io>

--- src/app/components/applications/SSV/RegisterValidatorHome/components/OfflineKeyShareGeneration/OfflineKeyShareGeneration.tsx
+++ src/app/components/applications/SSV/RegisterValidatorHome/components/OfflineKeyShareGeneration/OfflineKeyShareGeneration.tsx
@@ -13,7 +13,7 @@ import BorderScreen from '~app/components/common/BorderScreen';
 import ErrorMessage from '~app/components/common/ErrorMessage';
 import { validateAddressInput } from '~lib/utils/validatesInputs';
 import CustomTooltip from '~app/components/common/ToolTip/ToolTip';
-import { validateDkgAddress } from '~lib/utils/operatorMetadataHelper';
+import { isDkgAddressValid } from '~lib/utils/operatorMetadataHelper';
 import ProcessStore from '~app/common/stores/applications/SsvWeb/Process.store';
 import { CopyButton } from '~app/atomicComponents';
 import { getStoredNetwork } from '~root/providers/networkInfo.provider';
@@ -101,7 +101,7 @@ const OfflineKeyShareGeneration = () => {
   const processStore: ProcessStore = stores.Process;
   const operatorStore: OperatorStore = stores.Operator;
   const { apiNetwork } = getStoredNetwork();
-  const operatorsAcceptDkg = Object.values(operatorStore.selectedOperators).every((operator: IOperator) => !validateDkgAddress(operator.dkg_address ?? ''));
+  const operatorsAcceptDkg = Object.values(operatorStore.selectedOperators).every((operator: IOperator) => isDkgAddressValid(operator.dkg_address ?? ''));
   const isWindowOs = operatingSystemName === OperatingSystemsEnum.Windows;
   const dynamicFullPath = isWindowOs ? '%cd%' : '$(pwd)';
 

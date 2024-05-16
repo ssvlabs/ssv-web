722a02b318294042eb9ee25f4ce800b98c7627b0
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-05-16 17:22:06 +0300

    fix: wagmi networks, ssvload on unsupported chain

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -2,23 +2,32 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { Chain, HttpTransport, http } from 'viem';
 import { holesky, mainnet } from 'wagmi/chains';
 import { config as projectConfig } from '~app/common/config/config';
+import { networks } from '~root/providers/networkInfo.provider';
 
-const isFaucet = import.meta.env.VITE_FAUCET_PAGE;
-const isDistribution = import.meta.env.VITE_CLAIM_PAGE;
+// const isFaucet = import.meta.env.VITE_FAUCET_PAGE;
+// const isDistribution = import.meta.env.VITE_CLAIM_PAGE;
 
-const app = isFaucet ? 'faucet' : isDistribution ? 'distribution' : 'ssvweb';
+// const app = isFaucet ? 'faucet' : isDistribution ? 'distribution' : 'ssvweb';
 
-const appChains: Record<typeof app, [Chain, ...Chain[]]> = {
-  ssvweb: [mainnet, holesky],
-  distribution: [mainnet],
-  faucet: [holesky],
+// const appChains: Record<typeof app, [Chain, ...Chain[]]> = {
+//   ssvweb: [mainnet, holesky],
+//   distribution: [mainnet],
+//   faucet: [holesky]
+// };
+
+// const chains = appChains[app];
+
+const chainMap: Record<number, Chain> = {
+  [mainnet.id]: mainnet,
+  [holesky.id]: holesky
 };
+
+const chains = networks.map((network) => chainMap[network.networkId]).filter(Boolean) as [Chain, ...Chain[]];
 
 export const isChainSupported = (chainId: number) => {
   return chains.some((chain) => chain.id === chainId);
 };
 
-const chains = appChains[app];
 const transports = chains.reduce(
   (acc, chain) => {
     acc[chain.id] = http();

abef525fd809fb685b87be89e1b757a821c6bffb
Dima Axelrod <axerod@blox.io>
2024-05-20 15:10:11 +0300

    fixed lint

--- src/app/components/applications/SSV/MyAccount/components/Balance/Balance.tsx
+++ src/app/components/applications/SSV/MyAccount/components/Balance/Balance.tsx
@@ -1,21 +1,21 @@
 import Grid from '@mui/material/Grid';
-import {formatNumberToUi} from '~lib/utils/numbers';
+import { formatNumberToUi } from '~lib/utils/numbers';
 import NaDisplay from '~app/components/common/NaDisplay';
-import {translations} from '~app/common/config';
+import { translations } from '~app/common/config';
 import NewRemainingDays from '~app/components/applications/SSV/MyAccount/common/NewRemainingDays';
-import {useStyles} from '~app/components/applications/SSV/MyAccount/components/Balance/Balance.styles';
+import { useStyles } from '~app/components/applications/SSV/MyAccount/components/Balance/Balance.styles';
 import ErrorText from '~app/components/applications/SSV/MyAccount/common/LiquidationStateError/LiquidationStateError';
-import {fromWei} from '~root/services/conversions.service';
-import {ICluster} from '~app/model/cluster.model';
-import {PrimaryButton, SecondaryButton} from '~app/atomicComponents';
-import {ButtonSize} from '~app/enums/Button.enum';
+import { fromWei } from '~root/services/conversions.service';
+import { ICluster } from '~app/model/cluster.model';
+import { PrimaryButton, SecondaryButton } from '~app/atomicComponents';
+import { ButtonSize } from '~app/enums/Button.enum';
 import styled from 'styled-components';
 
 const OperationRunwayWrapper = styled.div`
     width: 100%
 `;
 
-const Balance = ({cluster, moveToReactivateCluster, moveToDeposit, moveToWithdraw}: {
+const Balance = ({ cluster, moveToReactivateCluster, moveToDeposit, moveToWithdraw }: {
   cluster: ICluster;
   moveToReactivateCluster: Function;
   moveToDeposit: Function;
@@ -37,7 +37,7 @@ const Balance = ({cluster, moveToReactivateCluster, moveToDeposit, moveToWithdra
             (<Grid item xs={12}
                    className={cluster.runWay < 30 ? classes.CurrentBalanceLiquidated : classes.CurrentBalance}>
               {formatNumberToUi(fromWei(cluster.balance))} SSV
-            </Grid>) : (<NaDisplay size={28} weight={800} text={translations.NA_DISPLAY.TOOLTIP_TEXT}/>)}
+            </Grid>) : (<NaDisplay size={28} weight={800} text={translations.NA_DISPLAY.TOOLTIP_TEXT} />)}
           <Grid item xs={12} className={classes.CurrentBalanceDollars}>
           </Grid>
         </Grid>
@@ -45,9 +45,9 @@ const Balance = ({cluster, moveToReactivateCluster, moveToDeposit, moveToWithdra
       {((Number(cluster.balance)) || cluster.isLiquidated) &&
         <OperationRunwayWrapper>
           {(cluster.validatorCount || cluster.isLiquidated) &&
-            <div><Grid item className={classes.SeparationLine} xs={12}/>
+            <div><Grid item className={classes.SeparationLine} xs={12} />
               <Grid container item className={classes.SecondSectionWrapper}>
-                <NewRemainingDays cluster={cluster}/>
+                <NewRemainingDays cluster={cluster} />
                 {cluster.isLiquidated && (
                   <Grid className={classes.ErrorMessageWrapper}>
                     <ErrorText
@@ -58,19 +58,19 @@ const Balance = ({cluster, moveToReactivateCluster, moveToDeposit, moveToWithdra
                 )}
               </Grid>
             </div>}
-          <Grid item className={classes.SeparationLine} xs={12}/>
+          <Grid item className={classes.SeparationLine} xs={12} />
           {cluster.isLiquidated ?
             (
               <Grid container item xs={12} className={classes.ActionButtonWrapper}>
-                <PrimaryButton text={'Reactivate Cluster'} onClick={moveToReactivateCluster} size={ButtonSize.XL}/>
+                <PrimaryButton text={'Reactivate Cluster'} onClick={moveToReactivateCluster} size={ButtonSize.XL} />
               </Grid>
             ) : (
               <Grid container item className={classes.ActionButtonWrapper}>
                 <Grid item xs>
-                  <PrimaryButton text={'Deposit'} onClick={moveToDeposit} size={ButtonSize.XL}/>
+                  <PrimaryButton text={'Deposit'} onClick={moveToDeposit} size={ButtonSize.XL} />
                 </Grid>
                 <Grid item xs>
-                  <SecondaryButton text={'Withdraw'} onClick={moveToWithdraw} size={ButtonSize.XL}/>
+                  <SecondaryButton text={'Withdraw'} onClick={moveToWithdraw} size={ButtonSize.XL} />
                 </Grid>
               </Grid>
             )}

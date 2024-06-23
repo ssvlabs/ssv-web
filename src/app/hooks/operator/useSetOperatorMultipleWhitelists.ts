887f143dd1b266aea90c605fa979ef1c4e934a71
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-23 16:17:22 +0300

    fix: request from enriched operator instead of delaying the operator refresh

--- src/app/hooks/operator/useSetOperatorMultipleWhitelists.ts
+++ src/app/hooks/operator/useSetOperatorMultipleWhitelists.ts
@@ -1,6 +1,9 @@
 import { UseMutationResult, useMutation } from '@tanstack/react-query';
+import { useAppSelector } from '~app/hooks/redux.hook';
 import { useTransactionExecutor } from '~app/hooks/useTransactionExecutor';
 import { EContractName } from '~app/model/contracts.model';
+import { getSelectedOperator } from '~app/redux/account.slice';
+import { getOperator } from '~root/services/operator.service';
 import { ContractMethod, getContractByName } from '~root/wagmi/utils';
 
 type MutationType = 'add' | 'delete';
@@ -22,6 +25,7 @@ const methods: Record<MutationType, ContractMethod> = {
 };
 
 export const useSetOperatorMultipleWhitelists = () => {
+  const operator = useAppSelector(getSelectedOperator);
   const executor = useTransactionExecutor();
 
   return useMutation({
@@ -29,7 +33,8 @@ export const useSetOperatorMultipleWhitelists = () => {
       return executor({
         contractMethod: methods[mode],
         payload: [operatorIds, addresses],
-        refreshMS: 29000
+        prevState: operator?.whitelist_addresses?.toSorted(),
+        getterTransactionState: async () => getOperator(operatorIds[0]).then((res) => res.whitelist_addresses?.toSorted())
       });
     }
   }) satisfies Mutation;

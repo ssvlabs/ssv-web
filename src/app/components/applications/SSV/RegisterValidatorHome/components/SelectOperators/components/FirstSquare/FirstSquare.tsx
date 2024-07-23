7e351d7470cf8aca637d5cede86c9ad1819433b4
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-07-23 19:24:40 +0300

    fix: operators scroll issue (#1091)

--- src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/FirstSquare.tsx
+++ src/app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/FirstSquare.tsx
@@ -1,60 +1,82 @@
-import { ReactElement, useEffect, useRef, useState } from 'react';
-import _ from 'underscore';
-import Grid from '@mui/material/Grid';
-import debounce from 'lodash/debounce';
-import styled from 'styled-components';
-import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
-import TableBody from '@mui/material/TableBody';
-import TableHead from '@mui/material/TableHead';
-import TableContainer from '@mui/material/TableContainer';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { TableFooter } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import debounce from 'lodash/debounce';
+import { forwardRef, useEffect, useMemo, useRef, useState } from 'react';
+import { useDebounce } from 'react-use';
+import { CustomContainerComponentProps, CustomItemComponentProps, Virtualizer } from 'virtua';
 import { translations } from '~app/common/config';
-import Spinner from '~app/components/common/Spinner';
-import { IOperator } from '~app/model/operator.model';
-import TextInput from '~app/components/common/TextInput';
-import { DEFAULT_PAGINATION } from '~app/common/config/config';
-import BorderScreen from '~app/components/common/BorderScreen';
-import { isDkgAddressValid } from '~lib/utils/operatorMetadataHelper';
-import { useAppDispatch, useAppSelector } from '~app/hooks/redux.hook';
-import GoogleTagManager from '~lib/analytics/GoogleTag/GoogleTagManager';
-import { getOperators as getOperatorsOperatorService } from '~root/services/operator.service';
 import { OperatorRow } from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/OperatorRow';
 import { useStyles } from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/FirstSquare.styles';
+import ClusterSize from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/ClusterSize/ClusterSize';
 import Filters from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/Filters';
 import StyledCell from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/StyledCell';
-import ClusterSize from '~app/components/applications/SSV/RegisterValidatorHome/components/SelectOperators/components/FirstSquare/components/ClusterSize/ClusterSize';
+import BorderScreen from '~app/components/common/BorderScreen';
+import Spinner from '~app/components/common/Spinner';
+import TextInput from '~app/components/common/TextInput';
+import { useAppDispatch, useAppSelector } from '~app/hooks/redux.hook';
+import { IOperator } from '~app/model/operator.model';
 import { fetchAndSetOperatorValidatorsLimit, getSelectedOperators, selectOperator, unselectOperator } from '~app/redux/operator.slice.ts';
+import GoogleTagManager from '~lib/analytics/GoogleTag/GoogleTagManager';
+import { isDkgAddressValid } from '~lib/utils/operatorMetadataHelper';
+import { cn } from '~lib/utils/tailwind';
+import { getOperators as getOperatorsOperatorService } from '~root/services/operator.service';
 
-const SpinnerWrapper = styled.div`
-  width: 100%;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+export const COLUMN_WIDTHS = [80, 180, 100, 150, 180, 100, 100];
 
 const FirstSquare = ({ editPage, clusterSize, setClusterSize, clusterBox }: { editPage: boolean; clusterSize: number; setClusterSize: Function; clusterBox: number[] }) => {
-  const [loading, setLoading] = useState(false);
   const [sortBy, setSortBy] = useState('');
   const [filterBy, setFilterBy] = useState([]);
   const [sortOrder, setSortOrder] = useState('');
   const [searchInput, setSearchInput] = useState('');
-  const [operatorsData, setOperatorsData] = useState<IOperator[]>([]);
-  const [operatorsPagination, setOperatorsPagination] = useState(DEFAULT_PAGINATION);
   const [dkgEnabled, selectDkgEnabled] = useState(false);
 
-  const filteredOperators = !dkgEnabled
-    ? operatorsData
-    : operatorsData.filter(({ dkg_address }) => {
-        return isDkgAddressValid(dkg_address ?? '');
+  const query = useInfiniteQuery({
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 5,
+    queryKey: ['choose-operators-list', sortBy, sortOrder, filterBy, searchInput, dkgEnabled],
+    queryFn: ({ pageParam }) => {
+      const ordering: string = `${sortBy ? `${sortBy}:${sortOrder}` : 'id:asc'}`;
+      return getOperatorsOperatorService({
+        page: pageParam,
+        ordering,
+        dkgEnabled,
+        perPage: 10,
+        type: filterBy,
+        search: searchInput
+      }).then((response) => {
+        if (pageParam === 1) scrollRef.current.scrollTop = 0;
+        return response;
       });
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      const { page, pages } = lastPage.pagination;
+      return page < pages ? page + 1 : undefined;
+    }
+  });
+
+  const operatorsData = useMemo(() => query.data?.pages.flatMap((page) => page.operators) ?? [], [query.data]);
+
+  const filteredOperators = useMemo(() => {
+    return !dkgEnabled
+      ? operatorsData
+      : operatorsData.filter(({ dkg_address }) => {
+          return isDkgAddressValid(dkg_address ?? '');
+        });
+  }, [dkgEnabled, operatorsData]);
 
   const scrollRef: any = useRef(null);
-  const classes = useStyles({ loading });
+  const classes = useStyles({ loading: query.isLoading });
   const dispatch = useAppDispatch();
   const selectedOperators = useAppSelector(getSelectedOperators);
+  const selectedOperatorsMap = useMemo(() => new Map(Object.values(selectedOperators).map((o) => [o.public_key, o])), [selectedOperators]);
 
   const headers = [
     { type: '', displayName: '' },
@@ -65,30 +87,6 @@ const FirstSquare = ({ editPage, clusterSize, setClusterSize, clusterBox }: { ed
     { type: 'mev', displayName: 'MEV', sortable: true },
     { type: '', displayName: '', sortable: true }
   ];
-
-  const getOperators = async (page: number) => {
-    if (page > operatorsPagination.pages && operatorsPagination.pages !== 0) return;
-    const ordering: string = `${sortBy ? `${sortBy}:${sortOrder}` : 'id:asc'}`;
-
-    const payload = {
-      page,
-      ordering,
-      dkgEnabled,
-      perPage: 10,
-      type: filterBy,
-      search: searchInput
-    };
-
-    const response = await getOperatorsOperatorService(payload);
-    if (response?.pagination?.page > 1) {
-      const operatorListInString = operatorsData.map((operator) => operator.id);
-      const operators = response.operators.filter((operator: any) => !operatorListInString.includes(operator.id));
-      setOperatorsData([...operatorsData, ...operators]);
-    } else {
-      setOperatorsData(response.operators);
-    }
-    setOperatorsPagination(response.pagination);
-  };
 
   const selectOperatorHandling = (operator: IOperator) => {
     if (Object.values(selectedOperators).some((selectedOperator: IOperator) => selectedOperator.id === operator.id)) {
@@ -128,40 +126,6 @@ const FirstSquare = ({ editPage, clusterSize, setClusterSize, clusterBox }: { ed
     }
   };
 
-  const dataRows = (): ReactElement | ReactElement[] => {
-    if (filteredOperators?.length === 0 && !loading) {
-      return (
-        <TableRow hover>
-          <StyledCell className={classes.NoRecordsWrapper}>
-            <Grid container>
-              <Grid item xs={12} className={classes.NoRecordImage} />
-              <Grid item xs={12} className={classes.NoRecordsText}>
-                No results found
-              </Grid>
-              <Grid item xs={12} className={classes.NoRecordsText}>
-                Please try different keyword or filter
-              </Grid>
-            </Grid>
-          </StyledCell>
-        </TableRow>
-      );
-    }
-
-    return filteredOperators.map((operator: IOperator) => {
-      const isSelected = Object.values(selectedOperators).some((selectedOperator: IOperator) => selectedOperator.id === operator.id);
-      return <OperatorRow key={operator.public_key} operator={operator} isSelected={isSelected} onClick={selectOperatorHandling} />;
-    });
-  };
-
-  const updateValue = _.debounce(() => {
-    if (loading) return;
-    if (operatorsPagination.page <= operatorsPagination.pages) {
-      const newPagination = Object.create(operatorsPagination);
-      newPagination.page += 1;
-      setOperatorsPagination(newPagination);
-    }
-  }, 200);
-
   const inputHandler = debounce((e) => {
     const userInput = e.target.value.trim();
     if (userInput.length >= 1 || userInput.length === 0) {
@@ -174,22 +138,20 @@ const FirstSquare = ({ editPage, clusterSize, setClusterSize, clusterBox }: { ed
     }
   }, 1000);
 
-  const rows = dataRows();
-
   useEffect(() => {
-    setLoading(true);
-    setOperatorsPagination(DEFAULT_PAGINATION);
-    getOperators(1).then(() => {
-      setLoading(false);
-      scrollRef.current.scrollTop = 0;
-    });
-  }, [searchInput, sortBy, sortOrder, filterBy]);
+    dispatch(fetchAndSetOperatorValidatorsLimit());
+  }, []);
 
-  useEffect(() => {
-    getOperators(operatorsPagination.page).then(async () => {
-      await dispatch(fetchAndSetOperatorValidatorsLimit());
-    });
-  }, [operatorsPagination.page]);
+  const [end, setEnd] = useState(0);
+  useDebounce(
+    () => {
+      if (end + 18 > filteredOperators.length && query.hasNextPage && !query.isFetching) {
+        query.fetchNextPage();
+      }
+    },
+    20,
+    [end]
+  );
 
   return (
     <BorderScreen
@@ -209,53 +171,92 @@ const FirstSquare = ({ editPage, clusterSize, setClusterSize, clusterBox }: { ed
                 withSideText
                 placeHolder={'Search...'}
                 onChangeCallback={inputHandler}
-                sideIcon={loading ? <CircularProgress size={25} className={classes.Loading} /> : <div className={classes.SearchIcon} />}
+                sideIcon={query.isLoading ? <CircularProgress size={25} className={classes.Loading} /> : <div className={classes.SearchIcon} />}
               />
             </Grid>
             <Filters setFilterBy={setFilterBy} dkgEnabled={dkgEnabled} selectDkgEnabled={selectDkgEnabled} />
           </Grid>
           <TableContainer id={'scrollableDiv'} ref={scrollRef} className={classes.OperatorsTable}>
-            <InfiniteScroll
-              dataLength={filteredOperators?.length}
-              next={() => {
-                updateValue();
-              }}
-              hasMore={operatorsData?.length !== operatorsPagination.total}
-              loader={
-                <SpinnerWrapper>
-                  <Spinner />
-                </SpinnerWrapper>
-              }
-              scrollableTarget={'scrollableDiv'}
-            >
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
+            {(query.isSuccess && !filteredOperators.length && (
+              <Table>
+                <TableBody>
                   <TableRow>
-                    {'length' in rows &&
-                      rows.length > 0 &&
-                      headers.map((header, index: number) => {
-                        const sortByType = sortBy === header.type;
-                        const ascending = sortOrder === 'asc';
-                        const descending = sortOrder === 'desc';
-                        let headerClasses = classes.SortArrow;
-                        if (sortByType) {
-                          if (ascending) headerClasses += ` ${classes.ArrowDown}`;
-                          if (descending) headerClasses += ` ${classes.ArrowUp}`;
-                        }
-                        return (
-                          <StyledCell key={index} className={classes.HeaderWrapper}>
-                            <Grid container onClick={() => header.sortable && sortHandler(header.type)}>
-                              <Grid item>{header.displayName}</Grid>
-                              {header.sortable && header.displayName !== '' && <Grid item className={headerClasses} />}
-                            </Grid>
-                          </StyledCell>
-                        );
-                      })}
+                    <StyledCell className={cn(classes.NoRecordsWrapper, 'border-b-0')}>
+                      <Grid container>
+                        <Grid item xs={12} className={classes.NoRecordImage} />
+                        <Grid item xs={12} className={classes.NoRecordsText}>
+                          No results found
+                        </Grid>
+                        <Grid item xs={12} className={classes.NoRecordsText}>
+                          Please try different keyword or filter
+                        </Grid>
+                      </Grid>
+                    </StyledCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>{rows}</TableBody>
+                </TableBody>
               </Table>
-            </InfiniteScroll>
+            )) || (
+              <Virtualizer
+                onRangeChange={(_, end) => setEnd(end)}
+                count={filteredOperators.length}
+                as={forwardRef<HTMLTableElement, CustomContainerComponentProps>(({ children, style }, ref) => {
+                  return (
+                    <Table
+                      ref={ref}
+                      style={{
+                        position: 'relative',
+                        height: style.height
+                      }}
+                      stickyHeader
+                      aria-label="sticky table"
+                    >
+                      <TableHead>
+                        <tr>
+                          {headers &&
+                            headers.map((header, index: number) => {
+                              const sortByType = sortBy === header.type;
+                              const ascending = sortOrder === 'asc';
+                              const descending = sortOrder === 'desc';
+                              let headerClasses = classes.SortArrow;
+                              if (sortByType) {
+                                if (ascending) headerClasses += ` ${classes.ArrowDown}`;
+                                if (descending) headerClasses += ` ${classes.ArrowUp}`;
+                              }
+                              const width = COLUMN_WIDTHS[index];
+                              return (
+                                <StyledCell key={index} className={classes.HeaderWrapper} style={{ width }}>
+                                  <Grid container onClick={() => header.sortable && sortHandler(header.type)}>
+                                    <Grid item>{header.displayName}</Grid>
+                                    {header.sortable && header.displayName !== '' && <Grid item className={headerClasses} />}
+                                  </Grid>
+                                </StyledCell>
+                              );
+                            })}
+                        </tr>
+                      </TableHead>
+                      <TableBody style={{ ...style }}>{children}</TableBody>
+
+                      <TableFooter>
+                        {query.isFetching && (
+                          <tr className="w-full h-12 relative">
+                            <div className="w-full flex justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                              <Spinner />
+                            </div>
+                          </tr>
+                        )}
+                      </TableFooter>
+                    </Table>
+                  );
+                })}
+                item={forwardRef<HTMLTableRowElement, CustomItemComponentProps>(({ index, style }, ref) => {
+                  const operator = filteredOperators[index];
+                  const isSelected = selectedOperatorsMap.has(operator.public_key);
+                  return <OperatorRow ref={ref} key={operator.public_key} style={style} operator={operator} isSelected={isSelected} onClick={selectOperatorHandling} />;
+                })}
+              >
+                {(i) => <>{i}</>}
+              </Virtualizer>
+            )}
           </TableContainer>
         </Grid>
       ]}

ef902147f524d9d87feab3c98314c1a8e3c9688a
Dima Axelrod <axelrod@ssvlabs.io>
2024-08-12 16:01:09 +0300

    added warning for keystore file

--- src/app/components/common/WarningBox/WarningBox.tsx
+++ src/app/components/common/WarningBox/WarningBox.tsx
@@ -1,19 +1,31 @@
 import Grid from '@mui/material/Grid';
 import { useStyles } from './WarningBox.styles';
 import LinkText from '~app/components/common/LinkText';
+import styled from 'styled-components';
 
 type InputProps = {
   text: string;
   link?: string;
   textLink?: string;
   extendClass?: any;
+  withLogo?: boolean;
 };
 
-const WarningBox = ({ text, link, textLink, extendClass }: InputProps) => {
+const WarningLogo = styled.div`
+  width: 46px;
+  height: 24px;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(/images/warning/warning.svg);
+`;
+
+const WarningBox = ({ text, link, textLink, extendClass, withLogo }: InputProps) => {
   const classes = useStyles();
 
   return (
     <Grid container className={`${classes.Wrapper} ${extendClass}`}>
+      {withLogo && <WarningLogo />}
       {text}
       {link && textLink && <LinkText text={textLink} link={link} />}
     </Grid>

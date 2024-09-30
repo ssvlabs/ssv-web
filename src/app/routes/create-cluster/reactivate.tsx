5ad750d5f3d65f631ed71050a609e88a21bd494a
Sumbat <sumbat@ssvlabs.io>
2024-09-30 14:56:32 +0300

    fix: reactive period options

--- src/app/routes/create-cluster/reactivate.tsx
+++ src/app/routes/create-cluster/reactivate.tsx
@@ -37,12 +37,16 @@ import { bigintifyNumbers, stringifyBigints } from "@/lib/utils/bigint";
 import { formatClusterData } from "@/lib/utils/cluster";
 import { sumOperatorsFee } from "@/lib/utils/operator";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { isEmpty, merge } from "lodash-es";
+import { merge } from "lodash-es";
 import type { ComponentPropsWithoutRef, FC } from "react";
 import { Collapse } from "react-collapse";
 import { useForm } from "react-hook-form";
 import { Navigate, useNavigate } from "react-router";
 import { z } from "zod";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Spacer } from "@/components/ui/spacer";
+import { formatSSV } from "@/lib/utils/number";
+import { cn } from "@/lib/utils/tw";
 
 export type ReactivateClusterProps = {
   // TODO: Add props or remove this type
@@ -54,8 +58,17 @@ type FCProps = FC<
 >;
 
 const schema = z.object({
-  days: z.coerce.number().positive().min(1),
+  selected: z.enum(["year", "half-year", "custom"]),
+  custom: z.coerce.number().positive().min(1),
 });
+
+const periods: Record<
+  Exclude<z.infer<typeof schema>["selected"], "custom">,
+  number
+> = {
+  year: 366,
+  "half-year": 182,
+};
 
 export const ReactivateCluster: FCProps = ({ ...props }) => {
   const navigate = useNavigate();
@@ -66,16 +79,41 @@ export const ReactivateCluster: FCProps = ({ ...props }) => {
   const operators = useOperators(operatorIds);
 
   const form = useForm<z.infer<typeof schema>>({
-    defaultValues: { days: 365 },
+    defaultValues: {
+      custom: 365,
+      selected: "year",
+    },
     resolver: zodResolver(schema),
   });
 
-  const days = form.watch("days");
+  const values = form.watch();
+  const days =
+    values.selected === "custom" ? values.custom : periods[values.selected];
 
-  const showLiquidationWarning =
-    !isEmpty(days) && days < globals.CLUSTER_VALIDITY_PERIOD_MINIMUM;
+  const showLiquidationWarning = Boolean(
+    days && days < globals.CLUSTER_VALIDITY_PERIOD_MINIMUM,
+  );
+
+  const customFundingCost = useFundingCost({
+    fundingDays: values.custom,
+    operators: operators.data ?? [],
+    validatorsAmount: cluster.data?.validatorCount ?? 1,
+  });
+
+  const yearFundingCost = useFundingCost({
+    fundingDays: periods.year,
+    operators: operators.data ?? [],
+    validatorsAmount: cluster.data?.validatorCount ?? 1,
+  });
+
+  const halfYearFundingCost = useFundingCost({
+    fundingDays: periods["half-year"],
+    operators: operators.data ?? [],
+    validatorsAmount: cluster.data?.validatorCount ?? 1,
+  });
 
   const computeFundingCost = useComputeFundingCost();
+
   const fundingCost = useFundingCost({
     operators: operators.data ?? [],
     validatorsAmount: cluster.data?.validatorCount ?? 1,
@@ -84,7 +122,8 @@ export const ReactivateCluster: FCProps = ({ ...props }) => {
 
   const reactive = useReactivate();
 
-  const submit = form.handleSubmit(async ({ days }) => {
+  const submit = form.handleSubmit(async ({ selected, custom }) => {
+    const days = selected === "custom" ? custom : periods[selected];
     const amount = await computeFundingCost.mutateAsync({
       fundingDays: days,
       operatorsFee: sumOperatorsFee(operators.data ?? []),
@@ -163,19 +202,80 @@ export const ReactivateCluster: FCProps = ({ ...props }) => {
           </Text>
           <FormField
             control={form.control}
-            name="days"
+            name="selected"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Days</FormLabel>
-                <FormControl>
-                  <NumberInput
-                    value={BigInt(field.value)}
-                    decimals={0}
-                    max={36500n}
-                    onChange={(value) => field.onChange(value.toString())}
-                  />
-                </FormControl>
-                <FormMessage />
+                <RadioGroup
+                  value={field.value}
+                  onValueChange={field.onChange}
+                  className="flex flex-col gap-2 [&>*]:w-full"
+                >
+                  <FormLabel htmlFor="year">
+                    <div className="flex items-center gap-3 border border-gray-400 rounded-lg py-[18px] px-6 outline outline-[4px] outline-none outline-offset-0 has-[input:checked]:border-primary-500 has-[input:checked]:outline-primary-100">
+                      <RadioGroupItem value="year" id="year" />
+                      <Text variant="body-2-semibold">1 Year</Text>
+                      <Spacer />
+                      <Text variant="body-1-bold">
+                        {formatSSV(yearFundingCost.data?.total ?? 0n)} SSV
+                      </Text>
+                    </div>
+                  </FormLabel>
+                  <FormLabel htmlFor="half-year">
+                    <div className="flex items-center gap-3 border border-gray-400 rounded-lg py-[18px] px-6 outline outline-[4px] outline-none outline-offset-0 has-[input:checked]:border-primary-500 has-[input:checked]:outline-primary-100">
+                      <RadioGroupItem value="half-year" id="half-year" />
+                      <Text variant="body-2-semibold">6 Months</Text>
+                      <Spacer />
+                      <Text variant="body-1-bold">
+                        {formatSSV(halfYearFundingCost.data?.total ?? 0n)} SSV
+                      </Text>
+                    </div>
+                  </FormLabel>
+                  <FormLabel htmlFor="custom">
+                    <div className="border border-gray-400 rounded-lg py-[18px] px-6 outline outline-[4px] outline-none outline-offset-0 has-[input:checked]:border-primary-500 has-[input:checked]:outline-primary-100">
+                      <div className="flex gap-3 items-center">
+                        <RadioGroupItem value="custom" id="custom" />
+                        <Text variant="body-2-semibold">Custom</Text>
+                        <Spacer />
+                        <Text variant="body-1-bold">
+                          {values.selected === "custom"
+                            ? formatSSV(customFundingCost.data?.total ?? 0n) +
+                              " SSV"
+                            : "-"}
+                        </Text>
+                      </div>
+                      <div
+                        className={cn(
+                          "transition-all duration-100",
+                          values.selected === "custom" ? "mt-4" : "mt-0",
+                        )}
+                      >
+                        <Collapse isOpened={values.selected === "custom"}>
+                          <FormField
+                            control={form.control}
+                            name="custom"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  <NumberInput
+                                    className="text-gray-800"
+                                    value={BigInt(field.value)}
+                                    onChange={(value) =>
+                                      field.onChange(Number(value))
+                                    }
+                                    decimals={0}
+                                    max={BigInt(365 * 9999)}
+                                    rightSlot={<>Days</>}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                        </Collapse>
+                      </div>
+                    </div>
+                  </FormLabel>
+                </RadioGroup>
               </FormItem>
             )}
           />

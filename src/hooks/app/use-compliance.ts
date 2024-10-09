83f532f744e0eea4979872e738f49c9d95fc4edf
Sumbat <sumbat@ssvlabs.io>
2024-10-09 13:37:42 +0300

    fix: compliance flag

--- src/hooks/app/use-compliance.ts
+++ src/hooks/app/use-compliance.ts
@@ -5,10 +5,14 @@ import {
 } from "@/api/ssv";
 import { ms } from "@/lib/utils/number";
 import { useQuery } from "@tanstack/react-query";
+import { useLocalStorage } from "react-use";
 import { useAccount } from "wagmi";
 
 export const useCompliance = () => {
+  const [disabled] = useLocalStorage("locationRestrictionDisabled", false);
+
   const account = useAccount();
+
   const userLocation = useQuery({
     queryKey: ["userLocation"],
     queryFn: getCurrentLocation,
@@ -21,9 +25,9 @@ export const useCompliance = () => {
   });
 
   return useQuery({
-    queryKey: ["compliance", account.chain, account.isConnected],
+    queryKey: ["compliance", account.chain, account.isConnected, disabled],
     queryFn: () => {
-      if (!account.isConnected || account.chain?.testnet) return "";
+      if (!account.isConnected || account.chain?.testnet || disabled) return "";
       return checkUserCountryRestriction(
         userLocation.data!,
         restrictedLocations.data!,

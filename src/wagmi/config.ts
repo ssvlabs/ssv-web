a2113ae688e4759be76db6110d7277650f2e423c
Ilya <ilya@blox.io>
2024-12-30 17:42:46 +0200

    change public rpc for mainnet

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -4,7 +4,6 @@ import {
   coinbaseWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 
-import type { HttpTransport } from "viem";
 import { createPublicClient, http } from "viem";
 import { createConfig } from "wagmi";
 import { holesky as holeskyBase, mainnet as mainnetBase } from "wagmi/chains";
@@ -29,14 +28,6 @@ export const isChainSupported = (chainId: number) => {
   return chains.some((chain) => chain.id === chainId);
 };
 
-const transports = chains.reduce(
-  (acc, chain) => {
-    acc[chain.id] = http();
-    return acc;
-  },
-  {} as Record<string, HttpTransport>,
-);
-
 const connectors = connectorsForWallets(
   [
     {
@@ -53,12 +44,17 @@ const connectors = connectorsForWallets(
 export const mainnet_private_rpc_client = createPublicClient({
   chain: mainnet,
   transport: http(
-    "https://misty-purple-sailboat.quiknode.pro/7fea68f21d77d9b54fc35c3f6d68199a880f5cf0",
+    "https://ethereum-rpc.publicnode.com/d8a2cc6e7483872e917d7899f9403d738b001c80e37d66834f4e40e9efb54a27",
   ),
 });
 
 export const config = createConfig({
   chains,
   connectors: connectors,
-  transports,
+  transports: {
+    [mainnet.id]: http(
+      "https://ethereum-rpc.publicnode.com/d8a2cc6e7483872e917d7899f9403d738b001c80e37d66834f4e40e9efb54a27",
+    ),
+    [holesky.id]: http(),
+  },
 });

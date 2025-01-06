2e3d36ff66ab804e2524c3a02aae059fe8bdf90f
Dima Axelrod <axelrod@ssvlabs.io>
2025-01-06 15:44:16 +0200

    fix: fixed badge color, added back to account for resign flow, fixed validators trashold

--- src/components/ui/completed-badge.tsx
+++ src/components/ui/completed-badge.tsx
@@ -1,6 +1,8 @@
+import { Text } from "@/components/ui/text.tsx";
+
 export const CompletedBadge = () => (
-  <div className="flex gap-2 items-center justify-center bg-success-300 text-sm font-medium text-success-500 py-1 px-2 rounded ">
-    Completed
-    <img className={"size-4"} src={`/images/step-done.svg`} />
+  <div className="flex gap-2 items-center justify-center bg-success-100 text-sm font-medium text-success-700 py-1 px-2 rounded ">
+    <Text className="ml-2">Completed</Text>
+    <img className={"size-4 mr-2"} src={`/images/step-done.svg`} />
   </div>
 );

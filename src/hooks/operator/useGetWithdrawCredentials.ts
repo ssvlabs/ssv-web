0e186314faaf55c8e4ff168e683255a88a9ca6ee
axelrod-ssvlabs <128966996+axelrod-blox@users.noreply.github.com>
2025-01-09 10:31:32 +0200

    Da/dkg reshare improvement (#1261)
    * fix: fixed bugs
    
    * fix: reshare flow clickable

--- src/hooks/operator/useGetWithdrawCredentials.ts
+++ src/hooks/operator/useGetWithdrawCredentials.ts
@@ -3,14 +3,39 @@ import { useQuery } from "@tanstack/react-query";
 import type { WithdrawCredentialResponse } from "@/api/validators.ts";
 import { getValidatorsWithdrawCredentials } from "@/api/validators.ts";
 
-export const useGetWithdrawCredentials = () => {
+export const useGetWithdrawCredentials = ({
+  setIsWithdrawalInputDisabled,
+  setWithdrawCredentials,
+}: {
+  setIsWithdrawalInputDisabled?: (res: boolean) => void;
+  setWithdrawCredentials?: (res: string) => void;
+}) => {
   const { proofsQuery } = useReshareDkg();
   return useQuery({
     queryKey: [proofsQuery?.data?.validators],
+    retry: false,
+    staleTime: 0,
+    gcTime: 0,
     enabled: !!proofsQuery?.data?.validators,
-    queryFn: (): Promise<WithdrawCredentialResponse> =>
-      getValidatorsWithdrawCredentials(
+    queryFn: async (): Promise<WithdrawCredentialResponse> => {
+      const res = await getValidatorsWithdrawCredentials(
         (proofsQuery?.data?.validators || []).map(({ publicKey }) => publicKey),
-      ),
+      );
+      if (setWithdrawCredentials) {
+        setWithdrawCredentials(
+          res.withdraw_credentials?.slice(26)
+            ? `0x${res.withdraw_credentials?.slice(26)}`
+            : "",
+        );
+      }
+      if (setIsWithdrawalInputDisabled) {
+        setIsWithdrawalInputDisabled(!!res.withdraw_credentials);
+      }
+      return {
+        withdraw_credentials: res.withdraw_credentials
+          ? `0x${res.withdraw_credentials?.slice(26)}`
+          : res.withdraw_credentials,
+      };
+    },
   });
 };

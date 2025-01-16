e3e1705d78014791c9d14b4c755922b05d277eb2
Dima Axelrod <axelrod@ssvlabs.io>
2025-01-16 11:12:09 +0200

    fix: fixed large payload issue

--- src/hooks/cluster/use-infinite-cluster-validators.ts
+++ src/hooks/cluster/use-infinite-cluster-validators.ts
@@ -5,10 +5,13 @@ import { filterOutRemovedValidators } from "@/lib/utils/cluster";
 import { getNextPageParam } from "@/lib/utils/infinite-query";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useChainId } from "wagmi";
+import { useBulkActionContext } from "@/guard/bulk-action-guard.tsx";
+import type { Validator } from "@/types/api.ts";
 
 export const useInfiniteClusterValidators = (
   clusterHash?: string,
   perPage = 10,
+  externalValidators?: string[],
 ) => {
   const params = useClusterPageParams();
   const hash = clusterHash || params.clusterHash;
@@ -37,6 +40,22 @@ export const useInfiniteClusterValidators = (
     fetchedValidators,
     removedOptimisticValidators.data || [],
   );
+
+  if (
+    activeValidators.length &&
+    externalValidators &&
+    !useBulkActionContext.state._selectedPublicKeys.length
+  ) {
+    const validatorsToUse = activeValidators.filter((validator: Validator) =>
+      externalValidators.some((truncatedPublicKey: string) =>
+        validator.public_key.includes(truncatedPublicKey),
+      ),
+    );
+
+    useBulkActionContext.state._selectedPublicKeys = validatorsToUse.map(
+      ({ public_key }) => public_key,
+    );
+  }
 
   const total = Math.max(
     (infiniteQuery.data?.pages[0]?.pagination.total || 0) -

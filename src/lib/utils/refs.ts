f656d14c9be4ae28796d902021c21908d6f633a3
Sumbat <sumbat15@gmail.com>
2025-04-02 13:08:27 +0300

    fix: percentage input on create strategy

---
+++ src/lib/utils/refs.ts
@@ -0,0 +1,18 @@
+import type * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function mergeRefs<T = any>(
+  refs: Array<
+    React.MutableRefObject<T> | React.LegacyRef<T> | undefined | null
+  >,
+): React.RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}

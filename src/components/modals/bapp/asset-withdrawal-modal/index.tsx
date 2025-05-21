cc8544db994fef8197710385dffe81d4da3ea4ed
Dima Axelrod <axelrod@ssvlabs.io>
2025-05-21 12:13:55 +0300

    fix: strategy asset withdrawal mixpanel

--- src/components/modals/bapp/asset-withdrawal-modal/index.tsx
+++ src/components/modals/bapp/asset-withdrawal-modal/index.tsx
@@ -31,7 +31,6 @@ import { withTransactionModal } from "@/lib/contract-interactions/utils/useWaitF
 import { toast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils/tw";
 import { Alert } from "@/components/ui/alert";
-import { track } from "@/lib/analytics/mixpanel";
 
 const formSchema = z.object({
   amount: z.bigint().min(BigInt(1), "Amount must be greater than 0"),
@@ -98,7 +97,6 @@ export const AssetWithdrawalModal = () => {
       assetWithdrawalRequest.invalidate();
       setTimeout(strategyQuery.invalidate, 2000);
       form.reset({ amount: BigInt(0) });
-      track("Strategy withdraw");
       return () => {
         navigate(`/account`);
       };

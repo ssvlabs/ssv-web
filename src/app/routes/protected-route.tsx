3d31a4b25897a5ccbe195c5fe0383704376e11ad
Sumbat <sumbat15@gmail.com>
2025-07-11 13:04:49 +0300

    fix: remove mainnet bapp blocker

--- src/app/routes/protected-route.tsx
+++ src/app/routes/protected-route.tsx
@@ -1,26 +1,17 @@
 import { useAccount } from "@/hooks/account/use-account";
 import { useCompliance } from "@/hooks/app/use-compliance";
 import type { ComponentPropsWithoutRef, FC } from "react";
-import { matchPath, Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute: FC<ComponentPropsWithoutRef<"div">> = ({
   ...props
 }) => {
-  const location = useLocation();
   const account = useAccount();
   const compliance = useCompliance();
 
   if (!account.isConnected) return <Navigate to="/connect" replace />;
   if (compliance.data) {
     return <Navigate to="/compliance" replace />;
-  }
-
-  const isTestnet = account.chain?.testnet;
-  const match = Boolean(matchPath("/account/*", location.pathname));
-  const shouldRedirectToRoot = Boolean(!isTestnet && match);
-
-  if (shouldRedirectToRoot) {
-    return <Navigate to="/" replace />;
   }
 
   return props.children;

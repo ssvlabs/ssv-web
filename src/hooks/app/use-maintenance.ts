19b412c3f4c34393d182b7c9ef27671986fdbd52
Dima Axelrod <axelrod@ssvlabs.io>
2025-09-11 15:17:22 +0300

    fix: changes

--- src/hooks/app/use-maintenance.ts
+++ src/hooks/app/use-maintenance.ts
@@ -4,17 +4,11 @@ import { useChainId } from "wagmi";
 export const useMaintenance = () => {
   const chainId = useChainId();
 
-  const initialValue = chainId !== 1;
+  const [isMaintenancePage] = useLocalStorage("isMaintenancePage", false, {
+    raw: false,
+    deserializer: (value) => Boolean(value),
+    serializer: (value) => String(value),
+  });
 
-  const [isMaintenancePage] = useLocalStorage(
-    "isMaintenancePage",
-    initialValue,
-    {
-      raw: false,
-      deserializer: (value) => Boolean(value),
-      serializer: (value) => String(value),
-    },
-  );
-
-  return { isMaintenancePage };
+  return { isMaintenancePage: chainId !== 1 || isMaintenancePage };
 };

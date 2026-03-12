615b862dfac0e04e22766241d3f6a9d544590b47
sumbat-ssvlabs <sumbat@ssvlabs.io>
2026-03-12 13:10:09 +0200

    chore: mock data for testing

--- src/hooks/cluster/use-cluster-effective-balance-breakdown.ts
+++ src/hooks/cluster/use-cluster-effective-balance-breakdown.ts
@@ -1,9 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { getValidatorsStatusCounts } from "@/api/validators";
+import { useLocalStorage } from "react-use";
+import type { ValidatorStatusFilterKey } from "@/lib/search-parsers/validators-search-parsers";
 
 export const useClusterEffectiveBalanceBreakdown = (clusterHash: string) => {
+  // TODO: remove this once tested
+  const [mockData] = useLocalStorage<
+    Record<ValidatorStatusFilterKey, number> | undefined
+  >("mockEbBreakdown", undefined);
+
   return useQuery({
-    queryKey: ["validators-status-counts", clusterHash],
-    queryFn: () => getValidatorsStatusCounts(clusterHash).then((res) => res.eb),
+    // TODO: remove mockData once tested
+    queryKey: ["validators-status-counts", clusterHash, mockData],
+    queryFn: () =>
+      mockData || getValidatorsStatusCounts(clusterHash).then((res) => res.eb),
   });
 };
